e3295e84ae456dc81e8954eb0d025df700d14ba4
vnwonah <talk2vinciii@hotmail.com>
2017-08-22 14:24:26 +0100

    Resolved Merge Cinflicts

--- library.php
+++ library.php
@@ -1,138 +1,68 @@
 <a name="top"></a>
-
 <?php include ('header.php'); ?>
-
 <link rel="stylesheet" href="css/style.css">
 
-
-<div id="library-header-content" class="roww clearfix">
-  <img src="images/banner-images/banner_edit.fw.png" alt="lin">
-  <div class="col-38">
-      <div class="section-heading">
-
-          <h1><span>HNG Interns' Library</span></h1>
-
-      </div>
-  </div>
-</div>
-
-
-<<<<<<< HEAD
-<!-- <div id="library-body-content" class="row clearfix" style="background-color:#fcfcfc;"> -->
-=======
-<input id="tab1" type="radio" name="tabs" checked>
-<label for="tab1">HTML/CSS</label>
-  
-<input id="tab2" type="radio" name="tabs">
-<label for="tab2">PHP/SQL</label>
-  
-<input id="tab3" type="radio" name="tabs">
-<label for="tab3">GIT/GitHub</label>
-  
-<input id="tab4" type="radio" name="tabs">
-<label for="tab4">Internship Tasks</label>
->>>>>>> 1abb39c914f7568f078d982fef8f1698fc8e4462
-
-<div>
-    <form id="library-search">
-      <input class="library-input" type="text" name="search" placeholder="Search..">
-    </form>
+  <div id="libraryHeader" name="libraryHeader">
+        <img id="libraryBanner" name="libraryBanner" src="https://fvtelibrary.com/img/images/temp/slider1_img.jpg" alt="e library image" />
   </div>
 
+  <h1 name="libraryheadertext">Hng.Fun Library</h1>
+</br>
+
+<main name="libraryMain" id="libraryMain">
+  
+  <input id="tab1" type="radio" name="tabs" checked>
+  <label for="tab1">HTML/CSS</label>
+    
+  <input id="tab2" type="radio" name="tabs">
+  <label for="tab2">PHP/SQL</label>
+    
+  <input id="tab3" type="radio" name="tabs">
+  <label for="tab3">GIT/GITHUB</label>
+    
+  <input id="tab4" type="radio" name="tabs">
+  <label for="tab4">Internship Tasks</label>
+
+  <section id="content1">
   <div class="library-all">
     <ul class="library-list">
-      <li>
-        <img class="library-img" src="images/library/gitbash.png">
-        <h3>Git Tutorials</h3>
-          <p><a href="https://backlogtool.com/git-guide/en/stepup/stepup1_1.html" target="_blank">- Git's beginner guide for dummies</a></p>
-          <p><a href="https://www.udacity.com/course/how-to-use-git-and-github--ud775" target="_blank">- Udacity's free course on How to use git and github</a></p>
-          <p><a href="https://git-scm.com/docs/gittutorial" target="_blank">- Git's own tutorial/documentation</a></p>
-          <p><a href="http://gitimmersion.com/" target="_blank">- Git Immersion</a></p>
-          <p><a href="http://www-cs-students.stanford.edu/~blynn/gitmagic/" target="_blank">- Git magic : Online book that takes you from beginner to pro.</a></p>
-          <p><a href="https://www.webdesignerdepot.com/2009/03/intro-to-git-for-web-designers/" target="_blank">- Intro to git for web designers</a></p>
-          <p><a href="" target="_blank">-Lorem ipsum dolor sit amet, consectetur adipiscing elit. </a></p>
-          <p><a href="" target="_blank">-Lorem ipsum dolor sit amet, consectetur adipiscing elit. </a></p>
-          <p><a href="" target="_blank">-Lorem ipsum dolor sit amet, consectetur adipiscing elit. </a></p>
-      </li>
-        
-      <li>
-        <img class="library-img" src="images/library/github.png">
-        <h3>Github Tutorials</h3>
-          <p><a href="http://opensourcerer.diy.org/" target="_blank">- Open Sourcerer : Takes you from your first repo to your first pull request</a></p>
-          <p><a href="https://help.github.com/">- Github's own tutorial</a></p>
-          <p><a href="https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/" target="_blank">- Adding a project to Github with Command Line</a></p>
-          <p><a href="https://help.github.com/articles/fetching-a-remote/" target="_blank">-Working with other people's repositories</a></p>
-          <p><a href="" target="_blank">- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-          <p><a href="" target="_blank">- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-          <p><a href="" target="_blank">- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-          <p><a href="" target="_blank">- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-          <p><a href="" target="_blank">- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-      </li>
-
-      <li>
-        <img class="library-img" src="images/library/mysq.png">
-        <h3>MySQL Tutorials</h3>
-          <p><a href="https://www.w3schools.com/SQL/deFault.asp" target="_blank">- W3Schools SQL Tutorial</a></p>
-          <p><a href="https://www.tutorialrepublic.com/sql-tutorial/" target="_blank">- Tutorial Republic SQL Tutorial</a></p>
-          <p><a href="https://www.tutorialspoint.com/mysql/index.htm" target="_blank">- Tutorials Point MySQL Tutorial</a></p>
-          <p><a href="https://www.digitalocean.com/community/tutorials/a-basic-mysql-tutorial" target="_blank">- A Basic MySQL Tutorial</a></p>
-          <p><a href="" target="_blank">- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-          <p><a href="" target="_blank">- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-          <p><a href="" target="_blank">- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-      </li>
-
-      <li>
-        <img class="library-img" src="images/library/php.jpg">
-        <h3>PHP Tutorials</h3>
-          <p><a href="https://www.w3schools.com/php/default.asp" target="_blank">- W3Schools PHP Tutorial</a></p>
-          <p><a href="http://php.net/manual/en/index.php" target="_blank">- PHP Documention in a simple manual format</a></p>
-          <p><a href="http://php.net/manual/en/tutorial.php" target="_blank">- A simple PHP tutorial</a></p>
-          <p><a href="https://www.tutorialspoint.com/php/" target="_blank">- Tutorials Point PHP Tutorial</a></p>
-          <p><a href="https://www.codecademy.com/learn/php" target="_blank">- Codecademy PHP Tutorial</a></p>
-          <p><a href="https://www.tutorialrepublic.com/php-tutorial/" target="_blank">- Tutorial Republic PHP Tutorial</a></p>
-          <p><a href="https://www.howtoforge.com/php_programming_basics" target="_blank">- PHP Programming Basics</a></p>
-          <p><a href="" target="_blank">- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></p>
-      </li>
-
-      
-      
+      <a href="http://www.tutorialspoint.com/html/" target="_blank">
+        <li>
+          <img class="library-imas" src="https://image.freepik.com/free-icon/html-file-with-code-symbol_318-45756.jpg">
+          <h3>HTML</h3>
+          <p>
+          HTML stands for Hyper Text Markup Language, which is the most widely used language on Web to develop web pages. 
+          HTML was created by Berners-Lee in late 1991 but "HTML 2.0" was the first standard HTML specification which was 
+          published in 1995. HTML 4.01 was a major version of HTML and it was published in late 1999. Though HTML 4.01 version is 
+          widely used but currently we are having HTML-5 version which is an extension to HTML 4.01, and this version was published in 2012.
+          </p>
+        </li>
+      </a>
+      <a href="https://www.w3schools.com/html/default.asp" target="_blank">
+        <li>
+          <img class="library-imas" src="http://blog.computercompany.net/wp-content/uploads/2014/07/HTML5logo.png">
+          <h3>HTML 5</h3>
+          <p> The language for building web pages </p></br>
+          <p>With HTML you can create your own Web site.
+            This tutorial teaches you everything about HTML 5.
+              HTML is easy to learn - You will enjoy it.</p>
+        </li>
+      </a>
+      <a href="https://mva.microsoft.com/en-us/training-courses/html5-css3-fundamentals-development-for-absolute-beginners-14207?l=Y4COscFfB_7500115888" target="_blank">
+        <li>
+          <img class="library-imas" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/CSS3_and_HTML5_logos_and_wordmarks.svg/2000px-CSS3_and_HTML5_logos_and_wordmarks.svg.png">
+          <h3>HTML 5 AND CSS 3</h3>
+          <p> HTML5 And CSS3 Fundamentals: Development for Absolute Beginners from Microsoft Virtual Academy </p></br>
+          <p>Want to learn HTML5 and CSS3? Over the course of 21 episodes, our friend Bob Tabor, from DevU.com, teaches you the fundamentals of HTML5 AND CSS3 programming. Tune in to learn CSS3 styles, concepts about webpages, and HTML5 features. 
+          
+         Explore HTML5 and get CSS3 training that includes web principles, writing code, and much more! Each concept is broken into its own video so you can search for and focus on the information you need to learn HTML5 and CSS3. This is a video series </p>
+        </li>
+      </a>
     </ul>
   </div>
-      
-  <!--<div class="library-title-h3"  >
-||||||| merged common ancestors
-<div id="library-body-content" class="row clearfix" style="background-color:#fcfcfc;">
-      
-  <div class="library-title-h3"  >
-=======
-<div id="library-body-content" class="row " style="background-color:#fcfcfc;">
- <div style=" width:100%; padding-left:10%; padding-right:10%; "  >
->>>>>>> ff09b7977d43f691f8994a9bd5591d37c7c0758a
-    <form class="">
-       <input class="inputss" type="text" name="search" placeholder="Search..">
-    </form>
-  </div>
-<<<<<<< HEAD
 
-   <div class="content-holder" class="clearfix">
-    </br>
-    </br>
-  <div class="library-title-h3"  >
+</section>
     
-<<<<<<< HEAD
-    <input id="tab1-library" type="radio" name="tabs-library" checked>
-    <label for="tab1-library">HTML</label>
-    
-    <input id="tab2-library" type="radio" name="tabs-library">
-    <label for="tab2-library">PHP</label>
-    
-    <input id="tab3-library" type="radio" name="tabs-library">
-    <label for="tab3-library">GIT/GitHub</label>
-    
-    <input id="tab4-library" type="radio" name="tabs-library">
-    <label for="tab4-library">HNG.Fun Tutorials</label>
-||||||| merged common ancestors
-=======
 <section id="content2">
 <div class="library-all">
 <ul class="library-list">
@@ -162,7 +92,6 @@
   
   </ul>
 </div>
-
 </section>
 
 <section id="content3">
@@ -194,38 +123,10 @@
   </a>
 </ul>
 </div>
->>>>>>> 1abb39c914f7568f078d982fef8f1698fc8e4462
-
-  <div class="content-holder" class="clearfix">
-    </br>
-    </br>
-  <div class="library-title-h3"  >
+</section>
     
-    <input id="tab1-library" type="radio" name="tabs-library" checked>
-    <label for="tab1-library">HTML</label>
-    
-<<<<<<< HEAD
-    <input id="tab2-library" type="radio" name="tabs-library">
-    <label for="tab2-library">PHP</label>
-    
-    <input id="tab3-library" type="radio" name="tabs-library">
-    <label for="tab3-library">GIT/GitHub</label>
-    
-    <input id="tab4-library" type="radio" name="tabs-library">
-    <label for="tab4-library">HNG.Fun Tutorials</label>
-=======
-	<section style="width:100%; padding-left:10%; padding-right:10%;">
-    <div style="width:100%; padding-left:10%; padding-right:60%; ">
-    <select style="width:100%">
-  <option value="PHP">PHP</option>
-  <option value="HTML">HTML</option>
-  <option value="CSS">CSS</option>
-  <option value="GIT/GITHUB">GIT/GITHUB</option>
-  <option value="HNG.Fun Tutorials">HNG.Fun Tutorials</option>
-</select>
->>>>>>> ff09b7977d43f691f8994a9bd5591d37c7c0758a
-=======
 <section id="content4">
+<div class="library-all">
 <div class="library-all">
 <ul class="library-list">
   <a href="https://medium.com/hng-internship/first-steps-for-the-internship-8157b26f4c26" target="_blank">
@@ -244,133 +145,14 @@
   </a>
   
 </ul>
->>>>>>> 1abb39c914f7568f078d982fef8f1698fc8e4462
 </div>
-    	<div style="">
-        
-          <a href="https://www.tutorialspoint.com/mysql/" target="_blank">
-           
-             <div style=" float:left; width:15%;"> <img class="library-imas" src="images/library/mysq.png" width="100" height="100"> <br /></div>
-            <div style=" float:right; width:85%; ">  <h3>mysql</h3>
-              <p>
-                SQL is one of the most important inventions in the software engineering world, 
-                equally important to the invention of the Internet.
-                Most of the Web applications today store their data in Relational Databases 
-                and SQL is the ubiquitous language that is used to do this.
-                No wonder its important to us that you have a basic knowledge of it. 
-              </p></div>
-           
-          </a><br /><br />
-          <a href="https://backlogtool.com/git-guide/en/stepup/stepup1_1.html" target="_blank">
-       <div style=" float:left; width:15%;">  <img class="library-imas" src="images/library/gitbash.png" width="100" height="100">
-           <br /></div><div style=" float:right; width:85%;">   <h3>Git tutorials</h3>
-              <p>Git's beginner guide for dummies, Lorem ipsum dolor sit amet, consectetur adipiscing elit</p></div>
-            
-         </a><br /><br /></div>
-        <div style="">
-         
-        
-        <a href="https://www.udacity.com/course/how-to-use-git-and-github--ud775" target="_blank"> 
-          <div style=" float:left; width:15%;">
-            <img class="library-imas" src="images/library/gitbash.png" width="100" height="100"><br /></div>
-           <div style=" float:right; width:85%;"> <h3>Git tutorials on udacity</h3>
-            <p>Udacity's free course on How to use git and github, Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-          </div>
-        </a><br /><br />
-        
-        </div>
-        <div style="">
-          
-        
-          
-          <a href="https://www.webdesignerdepot.com/2009/03/intro-to-git-for-web-designers/" target="_blank">
-            <div style=" float:left; width:15%;">
-              
-              </div>
-              <div style=" float:right; width:85%;">
-              <h3>Intro to Git</h3>
-              <p>Intro to git for web designers, Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-              </div>
-            
-          </a>
 
-        </div>
-        <div style="">
-        
-          <a href="https://www.tutorialspoint.com/mysql/" target="_blank">
-            <div style=" float:left; width:15%;">
-              <img class="library-imas" src="images/library/mysq.png" width="100" height="100"><br />
-              </div>
-              <div style=" float:right; width:85%;">
-              <h3>mysql</h3>
-              <p>Intro to Mysql for web designers, Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-              </div>
-            
-          </a>
-           <br /><br />
-          <a href="https://backlogtool.com/git-guide/en/stepup/stepup1_1.html" target="_blank">
-            <div style=" float:left; width:15%;">
-              <img class="library-imas" src="images/library/gitbash.png" width="100" height="100"><br />
-              </div>
-              <div style=" float:right; width:85%;">
-              <h3>Git tutorials</h3>
-              <p>Git's beginner guide for dummies, Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-              </div>
-            
-          </a>
-           <br /><br />
-          <a href="https://www.udacity.com/course/how-to-use-git-and-github--ud775" target="_blank"> 
-            <div style=" float:left; width:15%;">
-              <img class="library-imas" src="images/library/gitbash.png" width="100" height="100"><br />
-              </div>
-              <div style=" float:right; width:85%;">
-              <h3>Git tutorials on udacity</h3>
-              <p>Udacity's free course on How to use git and github, Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-              </div>
-             
-          </a>
-           <br /><br />
-          <a href="https://git-scm.com/docs/gittutorial" target="_blank">             
-            <div style=" float:left; width:15%;">
-              <img class="library-imas" src="images/library/gitbash.png" width="100" height="100"><br />
-              </div>
-              <div style=" float:right; width:85%;">
-              <h3>Git Documentation</h3>
-              <p>Git's own tutorial/documentation, Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-              </div>
-            
-          </a>
-           <br /><br />
-          <a href="https://www.codeschool.com/courses/mastering-github" target="_blank">
-            <div style=" float:left; width:15%;">
-              <img class="library-imas" src="images/library/github.png" width="100" height="100"><br />
-              </div>
-              <div style=" float:right; width:85%;">
-              <h3>Github</h3>
-              <p>understanding github, Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-              </div>
-          
-          </a>
-           <br /><br />
-        
-        
+</section>  
 
-        </div>
+    
+</main id="libraryMain">
 
-        </div>
-	</section>
-
-<<<<<<< HEAD
-    </section>  
-</div>
-</div> -->
-
-<<<<<<< HEAD
-<a href="#top" class="library-back-to-top" style="display: inline; font-size:36px; position:fixed; bottom:0; right:2;">
-  <i class="fa fa-arrow-circle-up fa-lg"></i>
-=======
 <a href="#top" class="library-back-to-top" style="display: inline;">
   <i class="fa fa-arrow-circle-up"></i>
->>>>>>> 0070ba5c7c94fccc95188bcb583ae2331faa5d74
 </a>
 <?php include ('footer.php'); ?>
