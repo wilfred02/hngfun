b6be5d8fcd07559e06818285562f5a4f16f20d3a
vicklight93 <victor.nwauwa93@gmail.com>
2017-08-22 18:14:13 +0100

    Victor added

--- participants.php
+++ participants.php
@@ -4396,6 +4396,27 @@
                 </div>
             </div>
         </div>
+		
+	    <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(https://avatars2.githubusercontent.com/u/17924533?v=4&s=400); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/vicklight93.html">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Victor Nwauwa</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">victor.nwauwa93@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 5 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
 <div class="p-card">
     <div class="v-center">
         <div class="p-card-photo" style="background-image: url(https://ca.slack-edge.com/T3QLSP8HM-U6Q4F9V0E-45a4b4ef6a53-512); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
