e712ce5377027699418cc3771bc396dc1d44f3dd
Orie Chinedu Emmanuel <oriechinedu@gmail.com>
2017-08-22 13:40:47 +0100

    added my profile

--- participants.php
+++ participants.php
@@ -4272,6 +4272,28 @@
    <!-- Each Participants ends here -->
 	    
 	    
+ <!--  Each Participant starts here  -->
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(https://avatars1.githubusercontent.com/u/30381435?v=4&s=460); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/oriechinedu.html">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">ORIE CHINEDU EMMANUEL</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">oriechinedu@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 8</h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!--  Each Participants ends here  -->
 	    
 	    
 
