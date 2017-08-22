b2527f0bc6ec74e991cc76df96f0640f898ec48c
Efe-Uririn Fregene <Efe-Uririn Fregene>
2017-08-22 10:58:56 +0100

    Added name to participants.php

--- participants.php
+++ participants.php
@@ -757,6 +757,27 @@
             </div>
         </div>
 
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(http://res.cloudinary.com/efefregene/image/upload/v1503395521/fregs1_dbxrmb.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/efefregene.html">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Fregene Efe-Uririn</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">efe.glory@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 8 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+
 
         <div class="p-card">
             <div class="v-center">
