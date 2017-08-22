3cd3b859b47c8e3595b5429dd30bc473f9c9b634
michaels <radpangolin@gmail.com>
2017-08-22 11:49:20 +0100

    add self to participants.php

--- participants.php
+++ participants.php
@@ -2346,6 +2346,31 @@
             </div>
         </div>
 
+        <!--  Each Participant starts here  -->
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url('https://ca.slack-edge.com/T3QLSP8HM-U3RKCT0NN-94f0f01b2d45-512'); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+
+                    <a href="http://hng.fun/profile/pangolin.html">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Okoko Michaels</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">radpangolin@gmail.com</p>
+                    <!--Email-->
+                </div>
+
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 7 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!--  Each Participants ends here  -->
+
             
 <div class="p-card">
     <div class="v-center">
