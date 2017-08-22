fd256463eeb839a0986d035eeada8973585a3c8b
Ukauwa David <dukauwa.du@gmail.com>
2017-08-22 14:11:27 +0100

    added name to participants page

--- participants.php
+++ participants.php
@@ -68,9 +68,32 @@
             </div>
         </div>
         <!--  Each Participants ends here  -->
+ <!--  Each Participant starts here  -->
+  <div class="p-card">
+         <div class="v-center">
+            <div class="p-card-photo" style="background-image: url(http://res.cloudinary.com/de14v8crs/image/upload/v1503358091/profile2_a7wxo3.jpg"); background-size: cover; background-repeat: no-repeat; background-position: center center;></div>
+            <div class="p-card-info">
+                <a href="http://hng.fun/profile/dukauwa">
+                    <!--Profile Link-->
+                    <h3 class="p-card-title">Ukauwa David </h3>
+                    <!--Profile Name-->
+                </a>
+                <p class="p-card-description">
+                    dukauwa.du@gmail.com
+                </p>
+                <!--Email-->
+            </div>
+            <div class="p-team">
+                <a href="#">
+                    <h3 class="p-card-title"> Team 1 </h3>
+                    <!--Team-->
+                </a>
 
+         </div>
+     </div>
+ </div>
 
-
+        <!--  Each Participants ends here  -->
  <!--  Each Participant starts here  -->
         <div class="p-card">
             <div class="v-center">
