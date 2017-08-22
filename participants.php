0ec7cba8ed0b94dd02404dcf97511aeddcb5276d
teejay13 <adetunjisalako@gmail.com>
2017-08-22 09:23:38 +0000

    teejay

--- participants.php
+++ participants.php
@@ -782,6 +782,30 @@
             </div>
         </div>
 
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(http://res.cloudinary.com/teejay/image/upload/v1503393694/tunji_vqbldh.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+
+
+                    <a href="http://hng.fun/profile/tobilight">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Salako Adetunji Abdulhaq</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">adetunjisalako@gmail.com</p>
+                    <!--Email-->
+                </div>
+
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 8 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+
 
         <div class="p-card">
             <div class="v-center">
@@ -2108,6 +2132,7 @@
                 </div>
             </div>
         </div>
+
 
         <!--  Each Participants ends here  -->
 
