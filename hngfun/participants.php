b46b54baf83703fc3dae94ffc5e0a01d22c3386f
KINGDEREL <kinsolomon9@gmail.com>
2017-08-23 00:01:11 +0100

    added my name

--- hngfun/participants.php
+++ hngfun/participants.php
@@ -986,4 +986,31 @@
         </div>
     </div>
 </div>
+
+<div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(https://avatars0.githubusercontent.com/u/29741635?v=4&u=5d29232abf5d2030a62b7427a988856735d1441f&s=400); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+
+
+                    <a href="http://hng.fun/profile/kingsolomon">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">KINGDEREL</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">kinsolomon9@gmail.com</p>
+                    <!--Email-->
+                </div>
+
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 1 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+        <!--  Each Participants ends here  -->
 <?php  include "footer.php" ?>
