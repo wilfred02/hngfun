4721094d5419e79df8305eabf0834dbf9ea65150
otu02 <ellipseshub@gmail.com>
2017-08-22 17:03:50 +0100

    Shield profile update

--- participants.php
+++ participants.php
@@ -2138,7 +2138,7 @@
 
         <div class="p-card">
             <div class="v-center">
-                <div class="p-card-photo" style="background-image: url(https://github.com/hnginterns/hngfun/blob/master/profile/shield/profile.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-photo" style="background-image: url(http://res.cloudinary.com/shield/image/upload/v1503417523/smart_t69r31.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
                 <div class="p-card-info">
                     <a href="http://hng.fun/profile/shield/profile.html">
                         <!--Profile Link-->
