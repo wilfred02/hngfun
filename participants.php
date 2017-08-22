9925fecb1e7b248c9d8c0cba566006b13b83b993
James James <Jamesjay4199@gmail.com>
2017-08-22 11:22:53 +0100

    Added my name to particiants

--- participants.php
+++ participants.php
@@ -2290,6 +2290,31 @@
                 </div>
             </div>
         </div>
+		
+		 <!--  Each Participant starts here  -->
+		<div class="p-card">
+			<div class="v-center">
+				<div class="p-card-photo" style="background-image: url(https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/19642700_889129831235503_1943970847228754113_n.jpg?oh=46bab93a071ea158c537b30dfb9283ed&oe=5A2D29E6); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+				<div class="p-card-info">
+
+					<a href="http://hng.fun/profile/jamesjay4199">
+						<!--Profile Link-->
+						<h3 class="p-card-title">James John James</h3>
+						<!--Profile Name-->
+					</a>
+					<p class="p-card-description">jamesjay4199@gmail.com</p>
+					<!--Email-->
+				</div>
+
+				<div class="p-team">
+					<a href="#">
+						<h3 class="p-card-title"> Team 6 </h3>
+						<!--Team-->
+					</a>
+				</div>
+			</div>
+		</div>
+		<!--  Each Participants ends here  -->
 
 			
 <div class="p-card">
