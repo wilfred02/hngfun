166d48e621a8e57bc0bc163223836df4116b5bdf
vic lotana <viclotana@gmail.com>
2017-08-22 16:44:59 +0100

    Added my details in participants

--- participants.php
+++ participants.php
@@ -3293,6 +3293,32 @@
 
         <!--  Each Participants ends here  -->
 
+         <!--  Each Participant starts here  -->
+
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(http://res.cloudinary.com/doab0xvfh/image/upload/v1503413511/WhatsApp_Image_2016-10-26_at_2.51.59_PM_rqabn2.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/viclotana">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Nwose Lotanna</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">viclotana@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 6 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!--  Each Participants ends here  -->
 
         <!--  Each Participant starts here  -->
 
