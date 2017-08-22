ba8e63607a7063e5325931ddda76bf71960d5ae6
Ajayi Thadeus <thadeus@shopspreeng.com>
2017-08-22 18:36:20 +0100

    Thadeus update participant file

--- participants.php
+++ participants.php
@@ -4376,6 +4376,35 @@
 </div>
    <!-- Each Participants ends here -->
 
+	<!-- Thadeus' profile -->
+
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(http://hng.fun/profile/thadeus/files/profile.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+
+
+                    <a href="http://hng.fun/profile/thadeus/thadeus.html">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Ajayi Thadeus</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">ainojie@gmail.com</p>
+                    <!--Email-->
+                </div>
+
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 1 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+        <!--  Each Participants ends here  -->
+
 	    <div class="p-card">
             <div class="v-center">
                 <div class="p-card-photo" style="background-image: url(https://avatars1.githubusercontent.com/u/22813358?v=4); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
