1d7e0048a1ddf1e61b4915715034983e114bcf1f
Kingsley Ukeje <30627493+swissking009@users.noreply.github.com>
2017-08-22 01:12:53 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -36,7 +36,7 @@
             <div class="footer-widget">
                 <div class="grid">
                     <div class="grid__col grid__col_12">
-                        <ul class="nav nav-bar footer-widget__icons">
+                        <ul class="footer-widget__icons">
                             <li>
                                 <a href=""><img src="img/icons/android.png" width="20"></a>
                             </li>
