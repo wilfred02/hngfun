8fda5c134641bbfecf3f5b6dfe2d716b44977295
Okorie Augustine <austinugo200@gmail.com>
2017-08-22 12:41:19 +0100

    Update participants.php

--- participants.php
+++ participants.php
@@ -1771,7 +1771,7 @@
 	     <!--  Each Participant starts here  -->
         <div class="p-card">
             <div class="v-center">
-                <div class="p-card-photo" style="background-image: url("https://avatars1.githubusercontent.com/u/24903349?v=4&s=460"); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-photo" style="background-image: url("https://avatars3.githubusercontent.com/u/24903349?v=4&u=99e547814677b11ee2d1e25c1f9e85191960ddb9&s=400"); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
                 <div class="p-card-info">
                     <a href="http://hng.fun/profile/auztincodes.html">
                         <!--Profile Link-->
