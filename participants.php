cddfdbac310539a57cd1e94f5d8d7bb3e046e277
Okorie Augustine <austinugo200@gmail.com>
2017-08-22 12:21:40 +0100

    Update participants.php

--- participants.php
+++ participants.php
@@ -1797,7 +1797,7 @@
 	     <!--  Each Participant starts here  -->
         <div class="p-card">
             <div class="v-center">
-                <div class="p-card-photo" style="background-image: url(""); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-photo" style="background-image: url("https://pbs.twimg.com/media/DHybTrJXkAcZZuB.jpg"); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
                 <div class="p-card-info">
                     <a href="http://hng.fun/profile/auztincodes.html">
                         <!--Profile Link-->
