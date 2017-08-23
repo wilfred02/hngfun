66ab9fd8b63a74a97a8f827319f1aca107e033f1
Philip <philipakpeki@gmail.com>
2017-08-23 01:50:06 +0100

    modify news.php again and adding min-width css property

--- news.php
+++ news.php
@@ -3,12 +3,17 @@
     <title>HNG Internship | News</title>
        <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <style>
+        #content{
+            min-height: 510px;
+        }
+    </style>
 </head>
 <body>
 <?php include ('header.php'); ?>
 
-
+<div id="content">
+</div>
 
 <?php include('footer.php'); ?>
 </body>
