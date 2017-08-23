db06ed6c5da65d7b628c4796177d74adb034ed70
Keresifon Ekpo <keresifon.ekpo@gmail.com>
2017-08-23 14:41:29 +0100

    participants.php

--- participants.php
+++ participants.php
@@ -6350,6 +6350,29 @@ include('header.php');
                             </div>
                         </div>
                         <!--  Each Participants ends here  -->
+						
+						<!-- Each participant starts here -->
+            <div class="p-card">
+                <div class="v-center">
+                    <div class="p-card-photo" style="background-image: url(http://bit.ly/2vbGpkH; background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                    <div class="p-card-info">
+                        <a href="http://hng.fun/profile/handsome9ice.html">
+                            <!--Profile Link-->
+                            <h3 class="p-card-title">Keresifon Ekpo</h3>
+                            <!--Profile Name-->
+                        </a>
+                        <p class="p-card-description">keresifon.ekpo@gmail.com</p>
+                    </div>
+                    <div class="p-team">
+                        <a href="#">
+                            <h3 class="p-card-title"> Team 9 </h3>
+                            <!--Team-->
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- Each participant ends here -->
+						
 
 
         </div>
