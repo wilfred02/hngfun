a8dad98c4f1da8ed7a2e419678e1db2e5a0b5195
Ijaware Tiwaloluwa <30860398+tiwalolu@users.noreply.github.com>
2017-08-24 00:31:18 +0100

    Update tiwaloluu.php

--- profile/tiwaloluu.php
+++ profile/tiwaloluu.php
@@ -14,7 +14,9 @@
 	 $exe = $con->query('SELECT * FROM password LIMIT 1');
 	 $data = $exe->fetch();
 	 $password = $data['password'];
-	 		   header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
+		 
+		 
+	 	     header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
 		}
 
 		  ?>
