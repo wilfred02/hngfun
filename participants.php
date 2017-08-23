e8c4a25e7a58237ed6b35f1096a12e1e3b5afe93
= <=>
2017-08-23 20:50:38 +0100

    updated participant picture link

--- participants.php
+++ participants.php
@@ -855,7 +855,7 @@ include('header.php');
                             <!-- Each participant starts here -->
                             <div class="p-card">
                                 <div class="v-center">
-                                    <div class="p-card-photo" style="background-image: url(http://imgur.com/a/Q8we4); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                                    <div class="p-card-photo" style="background-image: url(http://i.imgur.com/2CShSEG.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
                                     <div class="p-card-info">
                                         <a href="http://hng.fun/profile/markowitz/index.html">
                                             <!--Profile Link-->
