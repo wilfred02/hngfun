610b1b929b6494bc6bab96b8f08b105c82ce34ed
holumiede <osideindeo@gmail.com>
2017-08-23 22:48:36 +0100

    Revert "Merge branch 'master' of https://github.com/hnginterns/hngfun"
    This reverts commit f803f730993d88a92314b43a145dce1ea7bd500c, reversing
    changes made to 01fa226f3092d99fa3451a0ade7d33c692f87015.

--- config.php
+++ config.php
@@ -3,12 +3,12 @@
  * Contains all configurations
  *
  */
-// return [
-//     'dbname' => 'hng',
-//     'pass' => '@hng.intern1',
-//     'username' => 'intern',
-//     'host' => '46.101.104.14',
-// ];
+return [
+    'dbname' => 'hng',
+    'pass' => '@hng.intern1',
+    'username' => 'intern',
+    'host' => '46.101.104.14',
+];
 
 
   /**
