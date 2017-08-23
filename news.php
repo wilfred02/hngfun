ff9e72734c2e2088538602821a754898aa95d89a
Philip <philipakpeki@gmail.com>
2017-08-23 01:42:37 +0100

    modify news.php

--- news.php
+++ news.php
@@ -1,2 +1,14 @@
+<html>
+<head>
+    <title>HNG Internship | News</title>
+       <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
 <?php include ('header.php'); ?>
-<?php include ('footer.php'); ?>
+
+
+
+<?php include('footer.php'); ?>
+</body>
