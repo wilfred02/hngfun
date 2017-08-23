bd50e1e95e74e9d9526174670cad755ff87bcf0d
ksagoe <ksagoe@gmail.com>
2017-08-23 17:13:53 +0100

    fix error

--- profile/kay_s.php
+++ profile/kay_s.php
@@ -20,7 +20,7 @@
         $query = $db->query('SELECT * from password LIMIT 1');
         $data = $query->fetch();
         $password = $data['password'];
-        header("Location: ".$_SERVER['HTTP_HOST']."/sendmail.php?password=$password&subject=$subject&body=$body&to=ksagoe@gmail.com");
+        header("Location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=ksagoe@gmail.com");
         
     }
 ?>
