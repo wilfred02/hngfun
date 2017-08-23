d645c375a08ad6120b6e741e1246323e6dde405e
Bosun Egberinde <bosunski@gmail.com>
2017-08-23 01:18:49 +0100

    R

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+  /**
+   * Contains all configurations
+   *
+   */
+  return [
+    'dbname' => 'hng',
+    'pass' => '',
+    'username' => 'root',
+    'host' => 'localhost',
+    'mail_username' => 'hngstage2@gmail.com',
+    'mail_pass' => '12345asdfg',
+    'mail_host' => 'smtp.gmail.com',
+    'mail_port' => '456',
+  ];
