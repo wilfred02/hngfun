48d82d34cb64069bddb14ea96d8cb4711de19681
Michael Okoh <ordrizzy@gmail.com>
2017-08-23 16:28:43 +0100

    fix

--- profile/trojan/index.php
+++ profile/trojan/index.php
@@ -3,26 +3,26 @@
 
   if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $error = [];
-    $subject = $_POST['subject'];
+    $name = $_POST['name'];
     $to  = 'michael@okoh.co.uk';
-    $body = $_POST['message'];
+    $message = $_POST['message'];
 
-    if($body == '' || $body == ' ') {
+    if($message == '' || $message == ' ') {
       $error[] = 'Message cannot be empty.';
     }
 
-    if($subject == '' || $subject == ' ') {
-      $error[] = 'Subject cannot be empty.';
+    if($name == '' || $name == ' ') {
+      $error[] = 'name cannot be empty.';
     }
 
     if(empty($error)) {
-      $config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
+      $config = include(dirname(dirname(dirname(__FILE__))).'../../config.php');
       $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
       $con = new PDO($dsn, $config['username'], $config['pass']);
       $exe = $con->query('SELECT * FROM password LIMIT 1');
       $data = $exe->fetch();
       $password = $data['password'];
-      $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
+      $uri = "/sendmail.php?to=$to&message=$message&name=$name&password=$password";
       header("location: $uri");
 
     }
