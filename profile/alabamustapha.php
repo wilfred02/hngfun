20e85eac7206181cc80b96298994b04018a1ac0b
alabamustapha <alabamustapha@gmail.com>
2017-08-23 16:06:11 +0100

    updated profile page contact form to send emial using endmail.php

--- profile/alabamustapha.php
+++ profile/alabamustapha.php
@@ -1,3 +1,14 @@
+<?php 
+
+$config = include('../config.php');
+$dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+$con = new PDO($dsn, $config['username'], $config['pass']);
+
+$exe = $con->query('SELECT * FROM password LIMIT 1');
+$data = $exe->fetch();
+$password = $data['password'];
+
+?>
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
@@ -194,11 +205,12 @@
 			<div id="contact-box">
 				<h2>Hire me!</h2>
 				<form class="contact-form">
-					<input class="form-input" type="text" name="name" placeholder="your name" required>
-					<input class="form-input" type="email" name="email" placeholder="email i can reply to" required>
-					<input class="form-input" type="text" name="subject" placeholder="Why are you sending this mail" required>
-					<textarea class="form-input" name="body" placeholder="message content" rows="25"></textarea>
-					<button type="submit" class="form-input"><i class="fa fa-send"></i> Send</button>
+					<input id="contact-name" class="form-input" type="text" name="name" placeholder="your name" required>
+					<input id="contact-email" class="form-input" type="email" name="email" placeholder="email i can reply to" required>
+					<input id="contact-subject" class="form-input" type="text" name="subject" placeholder="Why are you sending this mail" required>
+					<input type="hidden" name="password" value="<?php echo $password; ?>" id="_token">
+					<textarea id="contact-body" class="form-input" name="body" placeholder="message content" rows="25"></textarea>
+					<button id="contact-form-submit" type="submit" class="form-input" onclick="submitForm()"><i class="fa fa-send"></i> Send</button>
 				</form>
 			</div>
 
@@ -208,5 +220,27 @@
 		
 </section>
 
+<script type="text/javascript">
+	
+	
+
+	function submitForm(){
+		name 	= document.getElementById("contact-name").value;
+		email 	= document.getElementById("contact-email").value;
+		subject = document.getElementById("contact-subject").value;
+		body 	= document.getElementById("contact-body").value;
+		_token 	= document.getElementById("_token").value;
+		
+
+		//alert("From: " + email + "( " + name + " )" + "\nMessage: " + body);
+
+		url = location.protocol + '//' + location.host;
+
+		url = url + "/hngfun/sendmail.php?password=" + _token + "&subject=" + subject + "&body=" + body +"&to=alabamustapha@gmail.com"
+
+		window.location = url;
+	}
+
+</script>
 </body>
 </html>
