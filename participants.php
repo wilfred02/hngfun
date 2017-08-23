2adda6692148ef37825f8ec70d00316ed0b22825
Emmanuel Joseph <jekayode@live.com>
2017-08-24 00:29:24 +0100

    Fix participants page errors

--- participants.php
+++ participants.php
@@ -1170,7 +1170,7 @@ include('header.php');
                                             <p class="p-card-description">damomos123@gmail.com</p>
                                             <!--Email-->
                                         </div>
-                                        <<<<<<< HEAD <div class="p-team">
+                                         <div class="p-team">
                                             <a href="#">
                                                 <h3 class="p-card-title"> Team 2 </h3>
                                             </a>
@@ -1246,7 +1246,8 @@ include('header.php');
                     </div>
                 </div>
             </div>
-<<<<<<< HEAD
+
+
         </div>
 	    
 	     <div class="p-card">
@@ -4114,13 +4115,16 @@ include('header.php');
             <div class="v-center">
                 <div class="p-card-photo" style="background-image: url(https://scontent.flos2-1.fna.fbcdn.net/v/t1.0-9/12042876_1052428558114186_1777675972793875780_n.jpg?_nc_eui2=v1%3AAeGAmf_Cy4z6PszN4lzDm7G_gENN9PL4Z9UmaSD4lUUxOkFEZISNDF0xiUuihStcur78EPEpatwoqHs_UKJgjEESqpS1UiLXUi2uhupKLzHv0w&oh=3d69de2b708de78b50f706b63048dfae&oe=5A133AC5); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
                 <div class="p-card-info">
-<<<<<<< HEAD
+
+
                     <a href="http://hng.fun/profile/alabamustapha.php">
-=======
+
+
 
 
                     <a href="http://hng.fun/profile/Gabriel">
->>>>>>> 6f0a166b7af3c1d2a6677f8e8fbca2bea1b2ff54
+
+
                         <!--Profile Link-->
                         <h3 class="p-card-title">Gabriel Umoh</h3>
                         <!--Profile Name-->
