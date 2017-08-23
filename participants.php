28e57edc747f8dbbf61e6900e64e9a60dab1ed99
Chikezie122 <emekys3@gmail.com>
2017-08-23 04:15:36 +0100

    Added new participant

--- participants.php
+++ participants.php
@@ -6282,6 +6282,28 @@ include('header.php');
         </div>
     </div>
     <!--  Each Participants ends here  -->
+                
+    <!--  Each Participant starts here  -->
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(http://hng.fun/profile/emekys3/images/DSC_5991.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/emekys3">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Nwadike Chikezie</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">emekys3@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!--  emekys3's profile  -->
 
 <<<<<<< HEAD
 <<<<<<< HEAD
