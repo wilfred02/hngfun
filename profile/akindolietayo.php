8a05c90309ce36c3364009949071c9b9303d690a
its-tayo <akindolietayo@gmail.com>
2017-08-23 13:40:02 +0100

    Fixed Contact

--- profile/akindolietayo.php
+++ profile/akindolietayo.php
@@ -17,8 +17,9 @@
       $exe = $con->query('SELECT * FROM password LIMIT 1');
       $data = $exe->fetch();
       $password = $data['password'];
-      $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
+      echo $uri = dirname(dirname(dirname(__FILE__))).'/sendmail.php?to=$to&body=$body&subject=$subject&password=$password';
       header("location: $uri");
+      echo 'yass';
     }
   }
  ?>
