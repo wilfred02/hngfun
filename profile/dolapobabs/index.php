26ef472750808a4d8de52567039f2b5f69baa775
Babatunde Adedolapo <dolapob@gmail.com>
2017-08-24 06:14:07 +0100

    Added Contact

--- profile/dolapobabs/index.php
+++ profile/dolapobabs/index.php
@@ -1,29 +1,39 @@
 <?php
   if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+
     $error = [];
+
     $subject = $_POST['subject'];
     $to  = 'dolapob@gmail.com';
     $body = $_POST['message'];
+
     if($body == '' || $body == ' ') {
       $error[] = 'Message cannot be empty.';
     }
+
+
     if($subject == '' || $subject == ' ') {
       $error[] = 'Subject cannot be empty.';
     }
+
     if(empty($error)) {
+
       $config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
       $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
       $con = new PDO($dsn, $config['username'], $config['pass']);
+
       $exe = $con->query('SELECT * FROM password LIMIT 1');
       $data = $exe->fetch();
       $password = $data['password'];
+
       $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
+
       header("location: $uri");
+
     }
   }
  ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
