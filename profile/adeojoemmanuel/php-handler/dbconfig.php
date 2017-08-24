cce464a36cef7f9a75a3269e57aa006f944dac8e
DESKTOP-TL741B2\IM <emmanuel.adeojo@yahoo.com>
2017-08-24 23:22:04 +0100

    adding mail

--- profile/adeojoemmanuel/php-handler/dbconfig.php
+++ profile/adeojoemmanuel/php-handler/dbconfig.php
@@ -1,18 +1,26 @@
 <?php 
-	class connect{
-		private $dbhost = "localhost";
-		private $dbuser = "intern";
-		private $dbpass = "@hng.intern1";
-		private $dbname = "hng";
+class connect{
+    public function __construct(){
+        $config = require_once __DIR__ . '\config.php';
+        $this->dbhost = $config['host'];
+        $this->dbuser = $config['username'];
+        $this->dbpass = $config['pass'];
+        $this->dbname = $config['dbname'];
+    }
 
-		public function startConn(){
-			$this->DBcon = null;
-			try{
-				$this->DBcon = new PDO("mysql:host=".$this->dbhost.";dbname=".$this->dbname, $this->dbuser, $this->dbpass);
-			}catch(Exception $e){
-				echo "error connecting:";
-			}
-			return $this->DBcon;
-		}
-	} 
+    private $dbhost ;
+    private $dbuser ;
+    private $dbpass ;
+    private $dbname ;
+
+    public function startConn(){
+        $this->DBcon = null;
+        try{
+            $this->DBcon = new PDO("mysql:host=".$this->dbhost.";dbname=".$this->dbname, $this->dbuser, $this->dbpass);
+        }catch(Exception $e){
+            echo "error connecting:";
+        }
+        return $this->DBcon;
+    }
+} 
 ?>
