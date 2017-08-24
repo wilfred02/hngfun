3bdcb52bff4b42503c073382f1d52ad56d73f7f5
ksagoe <ksagoe@gmail.com>
2017-08-24 23:26:40 +0100

    styling

--- profile/kay_s.php
+++ profile/kay_s.php
@@ -48,7 +48,7 @@
 
             h1 {
                 text-align: center;
-                color: #ADEFFF;
+                color: #22313F;
                 font-size: 58px;
             }
 
@@ -150,10 +150,13 @@
             }
 
             .body {
-                margin-top: 10px;
                 height: 300px;
                 resize: none;
                 line-height: 1.3;
+            }
+
+            .body-label {
+                margin-top: 10px;
             }
 
         </style>
@@ -175,8 +178,10 @@
                         <h2>Contact Me</h2>
                         <form action="./kay_s.php" method="post">
                             <input type="hidden" name="processing" value=true />
-                            <input type="text" class="textbox subject" name="subject" placeholder="Subject" /> 
-                            <textarea class="textbox body" name="body" placeholder="Message"></textarea>
+                            <label for="subject">Subject</label>
+                            <input type="text" class="textbox subject" name="subject" id="subject" /> 
+                            <label for="body" class="body-label">Message</label>
+                            <textarea class="textbox body" name="body" id="body"></textarea>
                             <input type="submit" value="Send" />
                             <?php if(isset($_POST['processing'])) : ?>
                                 <p>The Subject or Body field has not been entered</p>
