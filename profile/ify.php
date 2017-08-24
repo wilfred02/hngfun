e05c2789b64ba46e8d1923ddd688a6046c21d0a1
Alaba Mustapha <alabamustapha@gmail.com>
2017-08-24 20:24:49 +0100

    return php

--- profile/ify.php
+++ profile/ify.php
@@ -1,4 +1,25 @@
+<?php
+$host = "localhost";
+$username = "intern";
+$password = "@hng.intern1";
+$db = "hng";
+// Create connection
+$conn = mysql_connect($host, $username, $password);
+$db =  mysql_select_db($db);
+// Check connection
+if (!$conn) {
+    die("Connection failed");
+}
 
+
+$query = "SELECT * FROM password";
+//mysql_query($query) or die('Error querying database.');
+
+$result = mysql_query($query);
+$row = mysql_fetch_array($result);
+$password = $row['1'];
+
+?>
 
 <!DOCTYPE html>
 <html>
