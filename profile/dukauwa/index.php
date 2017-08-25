5b9f5587b73005c3ae5c764637a218bb062d577a
Ukauwa David <dukauwa.du@gmail.com>
2017-08-25 12:29:03 +0100

    Update

--- profile/dukauwa/index.php
+++ profile/dukauwa/index.php
@@ -101,7 +101,7 @@
               left: 50% ;
               transform: translate(-50%,-50%);
               width: 300px;
-              height:500px;
+              height:550px;
               background: #fff;
               box-sizing: border-box;
               border-radius: 10px;
