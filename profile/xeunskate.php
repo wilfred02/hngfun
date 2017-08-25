df4f4ae287bad08a889fd267ea23c9762f186c90
xeunskate <xeunskate@gmail.com>
2017-08-25 21:45:47 +0100

    change

--- profile/xeunskate.php
+++ profile/xeunskate.php
@@ -11,7 +11,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $error[] = 'Subject cannot be empty.';
     }
     if(empty($error)) {
-        $config = include(dirname(dirname(__FILE__))).'/config.php');
+        $config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
         $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
         $con = new PDO($dsn, $config['username'], $config['pass']);
         $exe = $con->query('SELECT * FROM password LIMIT 1');
