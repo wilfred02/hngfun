d4ead4bdb78beccb52030f28c24d4b7eeb88f49b
Keresifon Ekpo <keresifon.ekpo@gmail.com>
2017-08-25 23:50:28 +0100

    profile/handsome9ice.php

--- profile/handsome9ice.php
+++ profile/handsome9ice.php
@@ -1,14 +1,20 @@
-<?php 
-   $config = [
-     'dbname' => 'hng',
-     'pass' => '@hng.intern1',
-     'username' => 'intern',
-     'host' => 'localhost'
+<?php
+    if(isset($_POST['process'])){
+
+       $config = [
+            'dbname' => 'hng',
+            'pass' => '@hng.intern1',
+            'username' => 'intern',
+            'host' => 'localhost'
         ];
+
        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
         $con = new PDO($dsn, $config['username'], $config['pass']);
-		
-		$name = $_POST['name'];
+
+       $result = $con->query('SELECT * FROM password');
+        $data = $result->fetch();
+        $password = $data['password'];
+        $name = $_POST['name'];
 		$email = $_POST['email'];
 		$dropdown = $_POST['dropdown'];
 		$message = $_POST['message'];
@@ -16,6 +22,8 @@
 		$recipient = "keresifon.ekpo@gmail.com";
 		$subject = "Contact Form";
 		$mailheader = "From: $email \r\n";
-		mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-	echo "Thank You!";
+		mail($recipient, $subject, $formcontent, $mailheader)
+   }else{
+        header("location: handsome9ice.html");
+    }
 ?>
