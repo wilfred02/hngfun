0c37488a92b98136f5819af4cb7202b7dea4cb05
Ukauwa David <dukauwa.du@gmail.com>
2017-08-26 23:45:23 +0100

    update

--- profile/dukauwa/index.php
+++ profile/dukauwa/index.php
@@ -113,7 +113,6 @@
                      max-height: 200px;
                    }
                    .card-header img{
-                     height: 200px;
                      width: 100%;
                      object-fit: cover;
                    }
