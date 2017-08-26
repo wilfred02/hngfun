d4c1309f493137f0d20aafb7b688a9d8664e4bb2
damomos <damomos123@gmail.com>
2017-08-26 08:32:58 +0100

    Edited mail

--- profile/chiqokereke.php
+++ profile/chiqokereke.php
@@ -8,6 +8,7 @@
         $result = $con->query('SELECT * FROM password LIMIT 1');
         $data = $result->fetch();
 
+        $to = $_POST['to'];
         $password = $data['password'];
         $subject = $_POST['subject'];
         $body = $_POST['body'];
