6a0e6301f6fc9f3e7858991ac0bb814ce67dd2d2
rufai <rufai.mustopha@gmail.com>
2017-08-26 19:49:05 +0100

    something

--- profile/temi.php
+++ profile/temi.php
@@ -1,15 +1,4 @@
-<?php
-		$config = include('../config.php');
-        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-        $con = new PDO($dsn, $config['username'], $config['pass']);
-        $result = $con->query('SELECT * FROM password LIMIT 1');
-        $data = $result->fetch();
-        $to = $_POST['mail'];
-        $password = $data['password'];
-        $subject = $_POST['Subject'];
-        $body = $_POST['message'];
-        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to);
-?>
+
 <!DOCTYPE html>
 <html>
 <head>
