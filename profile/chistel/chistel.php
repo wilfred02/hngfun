d8357ba6beb92765377fe083bdc468f09e86dbc3
chistel <evameet@gmail.com>
2017-08-26 15:50:15 +0100

    updated

--- profile/chistel/chistel.php
+++ profile/chistel/chistel.php
@@ -1,25 +1,31 @@
 <?php 
-	$config = include('../config.php');
+   $config = [
+      'dbname' => 'hng',
+      'pass' => '@hng.intern1',
+      'username' => 'intern',
+      'host' => 'localhost'
+  	];
 	$dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
 	$con = new PDO($dsn, $config['username'], $config['pass']);
 	$exe = $con->query('SELECT * FROM password LIMIT 1');
 	$data = $exe->fetch();
+	$password = $data['password'];
 	$return = '';
 	if(isset($_POST['submit']))
 	{
-		$password = $data['password'];
+		//$password = $data['password'];
 		$subject = $_POST['subject'];
 		$message = $_POST['message'];
 		$to = 'chistelbrown@yahoo.com';
-		if(empty($password) && empty($subject) && empty($message))
+		/*if(empty($password) && empty($subject) && empty($message))
 		{
 			$return = "<div class='notification is-warning'>oh oh you did not fill all forms</div>";
-		}else{
+		}else{*/
 			
 			
 			$location = "../sendmail.php?password=".$password."&subject=".$subject."&body=".$message."&to=".$to;
 		   header("Location: " . $location);
-		}
+		//}
 	}
 ?>
 <!DOCTYPE html>
