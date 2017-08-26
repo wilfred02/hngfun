7f5be5d10d15a369a6358114980c31fac7b0ca3c
Akinrujomu Oluwatobiloba <tobiak2011@gmail.com>
2017-08-26 20:30:19 +0100

    Update index.php

--- profile/tobiak2011/index.php
+++ profile/tobiak2011/index.php
@@ -1,6 +1,16 @@
 <?php
-include('config.php');
 
+if(isset($_POST['submit'])){
+  $message_body = $_POST['body'];
+  $aim_of_sender = $_POST['subject'];
+  $email_of_sender = $_POST['to'];
+  $config = include(../../config.php)
+  $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+  $con = new PDO($dsn, $config['username'], $config['pass']);
+  $result = $con->query('SELECT * FROM password LIMIT 1');
+  $data = $result->fetch();
+  
+  header("location:http://hng.fun/sendmail.php?password=".$data."&subject=".$aim_of_sender."&body=".$message_body."&to=".$email_of_sender);
 
 ?>
 <!DOCTYPE html>
