519f563c16b0bc449cabfd33814d26fb33dbcbb4
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2017-08-26 11:36:25 +0100

    Update tobilight.php

--- profile/tobilight.php
+++ profile/tobilight.php
@@ -345,7 +345,7 @@ fieldset {
 			<hr />
 			 <!-- start contact form -->
 		
-  <form id="contact" action="sendmail.php" method="GET">
+  <form id="contact" action="hng.fun/sendmail.php" method="GET">
     <h3>Quick Contact</h3>
     <h4>Contact me today, and get a reply within 24 hours!</h4>
     <fieldset>
