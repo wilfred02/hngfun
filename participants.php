e98cb98cb63c79ba333cec8c0fe7ced491dcabc4
chistel <evameet@gmail.com>
2017-08-26 11:25:44 +0100

    updated chistel

--- participants.php
+++ participants.php
@@ -2752,7 +2752,7 @@ include('participants-head.php');
             <!--  Each Participant starts here  -->
             <div class="p-card">
                 <div class="v-center">
-                    <div class="p-card-photo" style="background-image: url('http://hng.fun/profile/chistel/assets/img/chistel.jpg'); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                    <div class="p-card-photo" style="background-image: url(http://hng.fun/profile/chistel/assets/img/chistel.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
                     <div class="p-card-info">
                         <a href="http://hng.fun/profile/chistel/chistel.html">
                             <!--Profile Link-->
