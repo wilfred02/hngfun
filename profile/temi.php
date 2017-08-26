b1a9016c1b9341451c722c919224d48fba93def9
rufai <rufai.mustopha@gmail.com>
2017-08-26 20:33:31 +0100

    updates

--- profile/temi.php
+++ profile/temi.php
@@ -7,13 +7,14 @@
 	$con = new PDO($dsn, $config['username'], $config['pass']);
 
 	$result = $con->query('SELECT * FROM password LIMIT 1');
-
 	$data = $result->fetch();
+
 	password = $data['password'];
 
     $to = $_POST['mail'];
-    $subject = $_POST['Subject'];
+    $subject = $_POST['subject'];
     $body = $_POST['message'];
+
 	header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to);
 
 ?>
