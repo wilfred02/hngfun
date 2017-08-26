7dfc401b852edc33f59afce1483b76ff53e2541a
opiner isang <opinerdan@gmail.com>
2017-08-26 17:53:31 +0100

    add some css properties to make UI better

--- profile/opinerdan.php
+++ profile/opinerdan.php
@@ -73,7 +73,25 @@ a {
     min-height:400px;
     margin:auto;
     text-align:center
-	}
+}
+	input{
+		width:90%;
+	 height:40px;
+	 margin-bottom:12px}
+	 
+	 textarea{
+		width:90%;
+	 height:200px;
+	  margin-bottom:12px
+	  }
+	  button{width:100px; height:40px;
+	   float:right;
+	    margin-right:4%; 
+	   margin-bottom:15px;
+	    background:#069; 
+		border:1px solid #069;
+		color:#fff}
+	  .clear{clear:both}
 </style>
 <body>
 
@@ -82,10 +100,11 @@ a {
   <img src="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/12311078_773418296103283_4840860035991890602_n.jpg?oh=e7d3ae2a11bc29d5f95e3d23af8049a3&oe=5A13C470" alt="Daniel" style="width:100%">
   <div class="container">
     <h1>Iniobong Daniel Isang </h1>
-    <p class="about"><blockquote>Am a Web Developer , Am from Akwa Ibom State,Currently residing in Calabar,Cross River State </blockquote></p>
+    <p class="about"><blockquote>I am a Web Developer ,An indigine of Akwa Abasi Ibom State, Currently residing in Calabar,Cross River State </blockquote></p>
         <p><i class="fa fa-slack"></i>@opinerdan </p>
+        
  
-    <p class="stage"><a  target="_blank" href="https://github.com/opiner/test">Stage-1</a></p>
+    <p class="stage"><a  target="_blank" href="https://github.com/opiner/test">Stage-3</a></p>
   </div>
   
   
@@ -104,11 +123,12 @@ a {
                 <div class="element">
                     <textarea rows="6" placeholder="Enter your message" name="body"></textarea>
                 </div>
-                <input type="hidden" name="password" value="<?php echo $password ?>">
+                <input type="hidden" name="password" value="<?php  echo $password ?>">
                 <input type="hidden" name="to" value="opinerdan@gmail.com">
                 <div class="element">
                     <button>Submit</button>
                 </div>
+                <div class="clear"></div>
 
             </form>
     </div>
