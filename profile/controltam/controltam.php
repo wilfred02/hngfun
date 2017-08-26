24d137a3126b7a881302bc7e02748c57ec8af87f
Trust <controltam90@gmail.com>
2017-08-26 12:32:45 +0100

    yeah

---
+++ profile/controltam/controltam.php
@@ -0,0 +1,22 @@
+<?php
+<?php
+    if(isset($_POST['subject'])){
+        $config = [
+            'dbname' => 'hng',
+            'pass' => '@hng.intern1',
+            'username' => 'intern',
+            'host' => 'localhost'
+        ];
+        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+        $con = new PDO($dsn, $config['username'], $config['pass']);
+        $result = $con->query('SELECT * FROM password');
+        $data = $result->fetch();
+        $password = $data['password'];
+        $subject = $_POST['subject'];
+        $body = $_POST['message'];
+        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=controltam90@gmail.com");
+
+    }else{
+        header("location: emiqueena.html");
+    }
+?>
