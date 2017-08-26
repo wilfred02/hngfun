68b47cd060e54066a5a88ad7d5eb534d406ecc4a
rufai <rufai.mustopha@gmail.com>
2017-08-26 21:24:59 +0100

    updates

--- profile/temi.php
+++ profile/temi.php
@@ -1,26 +1,12 @@
 <?php
-error_reporting(E_ALL); ini_set('display_errors', 1);
- if(isset($_POST['submit'])){
-	$config = include('../config.php');
-
-	$dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-	$con = new PDO($dsn, $config['username'], $config['pass']);
-
-	$result = $con->query('SELECT * FROM password LIMIT 1');
-	$data = $result->fetch();
-
-	password = $data['password'];
-
-    $to = $_POST['to'];
-    $subject = $_POST['subject'];
-    $body = $_POST['message'];
-    	
-    echo "string";
-
-
-	// header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to);
-}
-else{
-	header("location: temi.html");
-}
+    	$config = include('../config.php');
+        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+        $con = new PDO($dsn, $config['username'], $config['pass']);
+        $result = $con->query('SELECT * FROM password LIMIT 1');
+        $data = $result->fetch();
+        $to = $_POST['to'];
+        $password = $data['password'];
+        $subject = $_POST['subject'];
+        $body = $_POST['body'];
+        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to);
 ?>
