9e727f3be7b62815252956bd8aab2b04ec506637
Ajayi Thadeus <thadeus@shopspreeng.com>
2017-08-27 21:55:38 +0100

    Correction

--- profile/thadeus/thadeus.php
+++ profile/thadeus/thadeus.php
@@ -1,14 +1,14 @@
 <?php
 
-	$config = include('../config.php');
+/*	$config = include('../config.php');
     $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
     $con = new PDO($dsn, $config['username'], $config['pass']);
-
+*/
     $exe = $con->query('SELECT * FROM password LIMIT 1');
     $data = $exe->fetch();
     $password = $data['password'];
 
-  /*  if (isset($_GET['submit'])) {
+    if (isset($_GET['submit'])) {
 
         $subject = "Hello";
         $password = htmlentities(strip_tags(trim($password)));
@@ -31,7 +31,7 @@
 
     }
 	
-*/
+
 ?>
 
 <!DOCTYPE html>
