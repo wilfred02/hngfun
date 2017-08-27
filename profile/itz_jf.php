1a18d3633ed5ac3c8181980d184ca47d089720e0
Itz_JF <jesufemiodejide@gmail.com>
2017-08-27 16:59:36 +0100

    updated php

--- profile/itz_jf.php
+++ profile/itz_jf.php
@@ -29,7 +29,6 @@ if (isset($_GET['contact']))  {
 
     <title>Femi Odejide</title>
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
       .container {
