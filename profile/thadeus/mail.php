c4747fa38e464676337193c330c017bebbf88938
Ajayi Thadeus <thadeus@shopspreeng.com>
2017-08-27 21:30:39 +0100

    Fix page error on mail sending

--- profile/thadeus/mail.php
+++ profile/thadeus/mail.php
@@ -1,27 +1,35 @@
 <?php
 
-	if(isset($_POST['submit'])){
+	$config = include('../config.php');
+    $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+    $con = new PDO($dsn, $config['username'], $config['pass']);
 
-		$to = ainojie@gmail.com;//"xyluz@gmail.com";
-		$subject = "Contact from :" . $_POST['email'];
-		$email_field = $_POST['email'];
-		$message = $_POST['message'];
-		
-		$body = "From: $email_field\n Message:\n $message";
-		
-		$config = include(dirname(dirname(__FILE__)).'/config.php');
-		$dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-		$con = new PDO($dsn, $config['username'],$config['pass']);
-		
-		$exe = $con->query('SELECT * FROM password LIMIT 1');
-		$data = $exe->fetch();
-		$password = $data['password'];
-		
-		header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
-		
-	}
+    $exe = $con->query('SELECT * FROM password LIMIT 1');
+    $data = $exe->fetch();
+    $password = $data['password'];
 
-	
+    if (isset($_GET['submit'])) {
+
+        $subject = "Hello";
+        $password = htmlentities(strip_tags(trim($password)));
+        $body = htmlentities(strip_tags(trim($_GET['message'])));
+        $to = "ainojie@gmail.com";
+
+        if (!isset($body) || $body == '' || (!isset($_GET['email'])) || $_GET['message'] == '' ) {
+
+            echo "Form cannot be blank";
+
+        }else {
+
+            $location = "../sendmail.php?to=$to&subject=$subject&password=$password&body=$body";
+
+            header("Location: " . $location);
+
+
+        }
+
+
+    }
 	
 
 ?>
