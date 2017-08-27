b355535d220b768f0c744f9fb17483a513ca6a55
Ahmed Adisa <adisa.ahmed881@gmail.com>
2017-08-26 21:21:42 -0400

    My profile added

--- hngfun/participants.php
+++ hngfun/participants.php
@@ -1056,4 +1056,27 @@
             </div>
         </div>
         <!--  Each Participants ends here  -->
+
+        <!--  Each Participant starts here  -->
+            <div class="p-card">
+                <div class="v-center">
+                    <div class="p-card-photo" style="background-image: url(https://avatars1.githubusercontent.com/u/30381435?v=4&s=460); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                    <div class="p-card-info">
+                        <a href="http://hng.fun/profile/adisaahmed/amco.html">
+                            <!--Profile Link-->
+                            <h3 class="p-card-title">Ahmed Adisa</h3>
+                            <!--Profile Name-->
+                        </a>
+                        <p class="p-card-description">adisa.ahmed881@gmail.com</p>
+                        <!--Email-->
+                    </div>
+                    <div class="p-team">
+                        <a href="#">
+                            <h3 class="p-card-title"></h3>
+                            <!--Team-->
+                        </a>
+                    </div>
+                </div>
+            </div>
+        <!--  Each Participants ends here  -->
 <?php  include "footer.php" ?>
