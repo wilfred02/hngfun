6358bf1c39cb2af9f481048c12c753f800ac5268
Tim <fabrobocomx@gmail.com>
2017-08-27 08:16:35 +0100

    DEbug

--- profile/timolin/mailer.php
+++ profile/timolin/mailer.php
@@ -39,5 +39,5 @@ if (session_status() == PHP_SESSION_NONE) {
 
 $_SESSION['message'] = $result;
 
-header("Location: http://hng.fun/profile/timolin/timolin");
+header("Location: http://hng.fun/profile/timolin/timolin.php");
 exit();
