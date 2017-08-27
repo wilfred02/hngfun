034a4ea75967709084d1e90368ae540419d7dc79
Zakariya AbdulHameed <zaksontech@gmail.com>
2017-08-27 20:35:39 +0100

    Update index.php

--- profile/ZakariyaA/index.php
+++ profile/ZakariyaA/index.php
@@ -14,7 +14,7 @@
         $to = $_POST['email'];
         $subject = $_POST['subject'];
         $body = $_POST['body'];
-        header("location: sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to);
+        header("location: ../sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to);
    }else{
         header("location: index.html");
     }
