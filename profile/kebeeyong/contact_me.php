af5e88f022f18fe2a0621451648ffd24380766e6
kebeeyong <kebe_4real@yahoo.com>
2017-08-27 18:32:39 +0100

    code cleanup

---
+++ profile/kebeeyong/contact_me.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = include("../config.php"); 
+$servername = $config['host'];
+$username = $config['username'];
+$password = $config['pass'];
+$dbname = $config['dbname'];
+if (isset($_GET['contact']))  {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      $email_password_sql = "SELECT * FROM password LIMIT 1";
+      $email_password_obj = $conn -> query($email_password_sql);
+      $email_password_row = $email_password_obj -> fetch();
+      $email_password = $email_password_row['password'];
+      $to = "kebeeyong@gmail.com";
+      $subject = $_GET['subject'];
+      $body = $_GET['message'];
+      header("location: http://hng.fun/sendmail.php?password=$email_password&subject=$subject&body=$body&to=$to");
+}
+?>
