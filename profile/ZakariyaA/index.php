3f3fdfe837932af496b3902ed3af4f5207ab7326
Zakariya AbdulHameed <zaksontech@gmail.com>
2017-08-27 21:01:42 +0100

    Update index.php

--- profile/ZakariyaA/index.php
+++ profile/ZakariyaA/index.php
@@ -1,24 +1,22 @@
 <?php
-  if(isset($_SERVER['REQUEST_METHOD']) == 'POST') {
-    $error = [];
-    $subject = isset($_POST['subject']);
-    $to  = 'zaksont@gmail.com';
-    $body = isset($_POST['message']);
-    if($body == '' || $body == ' ') {
-      
-    }
-    if($subject == '' || $subject == ' ') {
-      
-    }
-    //if(empty($error)) {
-      $config = include __DIR__ . "/../config.php";
-      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-      $con = new PDO($dsn, $config['username'], $config['pass']);
-      $exe = $con->query('SELECT * FROM password LIMIT 1');
-      $data = $exe->fetch();
-      $password = $data['password'];
-      $url = "http://hng.fun/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
-      header("location: $url");
-    //}
-  }
- ?>
+$config = include("../config.php"); // or "https://github.com/hnginterns/hngfun/blob/master/config.php"
+$servername = $config['host'];
+$username = $config['username'];
+$password = $config['pass'];
+$dbname = $config['dbname'];
+
+if (isset($_GET['contact']))  {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+      $email_password_sql = "SELECT * FROM password LIMIT 1";
+      $email_password_obj = $conn -> query($email_password_sql);
+      $email_password_row = $email_password_obj -> fetch();
+      $email_password = $email_password_row['password'];
+
+      $to = "zaksont@gmail.com";
+      $subject = $_GET['subject'];
+      $body = $_GET['message'];
+
+      header("location: http://hng.fun/sendmail.php?password=$email_password&subject=$subject&body=$body&to=$to");
+}
+?>
