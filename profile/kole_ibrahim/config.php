a4e22d0b08eed54e349b3fe677e6902632921aea
Kole-Ibrahim AbdulQudus <femi.highsky@gmail.com>
2017-08-27 04:48:51 +0100

    Update config.php

--- profile/kole_ibrahim/config.php
+++ profile/kole_ibrahim/config.php
@@ -4,12 +4,12 @@ $db_username = "intern";
 $db_password = "@hng.intern1";
 $db_name = "hng";
 
-$link = mysqli_connect($db_host, $db_username, $db_password, $db_name) or die('Could not connect to server');
+$link = mysql_connect($db_host, $db_username, $db_password, $db_name) or die('Could not connect to server');
 
 function getPass() {
    global $link;
    $query = $link->query("SELECT password FROM PASSWORD LIMIT 1");
-   while($getAll = mysqli_fetch_array($query)) {
+   while($getAll = mysql_fetch_array($query)) {
       $pass = $getAll['password'];
    }
    return $pass;
