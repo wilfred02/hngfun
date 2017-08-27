90f9cf79255633ed60cb4acbd18190e4ebf8c778
peggylicious <peggylicious>
2017-08-28 00:11:27 +0100

    remove symbol

--- participants.php
+++ participants.php
@@ -24,7 +24,7 @@ include('participants-head.php');
 
 
 <!--  Each Participant starts here  -->
-#<div class="p-card">
+<div class="p-card">
     <div class="v-center">
         <div class="p-card-photo" style="background-image: url(http://i.imgur.com/M2buGfY.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
         <div class="p-card-info">
