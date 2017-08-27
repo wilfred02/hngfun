88faece9620645e8f760d6927604129cd7cad6df
Emmanuel Joseph <jekayode@live.com>
2017-08-27 19:00:33 +0100

    Removed framework from my profile

--- profile/jeni/contact.php
+++ profile/jeni/contact.php
@@ -1,3 +1,34 @@
 <?php
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $error = [];
 
-?>
+    $subject = $_POST['subject'];
+    $to  = 'jekayode@live.com';
+    $body = $_POST['message'];
+
+    if($body == '' || $body == ' ') {
+      $error[] = 'Please enter the message';
+    }
+
+
+    if($subject == '' || $subject == ' ') {
+      $error[] = 'Please enter the subject.';
+    }
+
+    if(empty($error)) {
+
+      $config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
+      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+      $con = new PDO($dsn, $config['username'], $config['pass']);
+
+      $exe = $con->query('SELECT * FROM password LIMIT 1');
+      $data = $exe->fetch();
+      $password = $data['password'];
+
+      $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
+
+      header("location: $uri");
+
+    }
+  }
+ ?>
