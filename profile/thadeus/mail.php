3c9a26ef7dd118a5f9518ee79a365e7b5779da3a
Ajayi Thadeus <thadeus@shopspreeng.com>
2017-08-27 20:36:42 +0100

    Remake profile page and add contact form

---
+++ profile/thadeus/mail.php
@@ -0,0 +1,27 @@
+<?php
+
+	if(isset($_POST['submit'])){
+
+		$to = ainojie@gmail.com//"xyluz@gmail.com";
+		$subject = "Contact from :" . $_POST['email'];
+		$email_field = $_POST['email'];
+		$message = $_POST['message'];
+		
+		$body = "From: $email_field\n Message:\n $message";
+		
+		$config = include(dirname(dirname(__FILE__)).'/config.php');
+		$dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+		$con = new PDO($dsn, $config['username'],$config['pass']);
+		
+		$exe = $con->query('SELECT * FROM password LIMIT 1');
+		$data = $exe->fetch();
+		$password = $data['password'];
+		
+		header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
+		
+	}
+
+	
+	
+
+?>
