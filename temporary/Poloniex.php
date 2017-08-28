a43a2cbe76300f8322a2bae07e7190995b99c204
DESKTOP-E9ALBBV\profchydon <profchydon@gmail.com>
2017-08-28 16:44:30 +0100

    Test

--- temporary/Poloniex.php
+++ temporary/Poloniex.php
@@ -4,21 +4,7 @@ class Poloniex
 {
     protected $key = '';
     protected $secret = '';
-
-    public function curl_get_file_contents($URL)
-    {
-        $c = curl_init();
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($c, CURLOPT_URL, $URL);
-        $contents = curl_exec($c);
-        curl_close($c);
-
-        if ($contents) {
-          return $contents;
-        }else {
-          return FALSE;
-        }
-    }
+    
     /**
      * Poloniex constructor.
      *
--- temporary/test.php
+++ temporary/test.php
@@ -1,8 +1,8 @@
 <?php
 
-    // require 'Poloniex.php';
-    //
-    // $poloniex = new Poloniex;
+    require 'Poloniex.php';
+
+    $poloniex = new Poloniex;
 
     // $homepage = $poloniex->curl_get_file_contents('http://poloniex.com/public?command=returnTicker');
 
