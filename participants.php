dde8681e8d27782f2087537d7a765a7099c00a55
Alimi Kehinde Morufudeen <alimi.kmaruf@gmmail.com>
2017-08-28 17:24:16 +0100

    making corrections

--- participants.php
+++ participants.php
@@ -73,9 +73,9 @@ include('participants-head.php');
 <!--  Each Participant starts here  -->
 <div class="p-card">
     <div class="v-center">
-        <div class="p-card-photo" style="background-image: url(http://hng.fun/profile/maruf/alimi.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+        <div class="p-card-photo" style="background-image: url(http://hng.fun/maruf/alimi.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
         <div class="p-card-info">
-            <a href="http://hng.fun/profile/maruf/marusoft.html">
+            <a href="http://hng.fun/maruf/marusoft.html">
                 <!--Profile Link-->
                 <h3 class="p-card-title">Alimi Kehinde Morufudeen</h3>
                 <!--Profile Name-->
