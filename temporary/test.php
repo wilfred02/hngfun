0798cb06d044bdc13615af138794d885f1e19642
DESKTOP-E9ALBBV\profchydon <profchydon@gmail.com>
2017-08-28 16:36:52 +0100

    Test

--- temporary/test.php
+++ temporary/test.php
@@ -7,7 +7,12 @@
     // $homepage = $poloniex->curl_get_file_contents('http://poloniex.com/public?command=returnTicker');
 
     $homepage = file_get_contents('http://poloniex.com/public?command=returnTicker');
-    echo $homepage;
+
+    $homepage = json_decode($homepage);
+
+    echo "<pre>";
+
+    var_dump($homepage);
 
     // echo "Test";
 
