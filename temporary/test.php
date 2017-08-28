52d5e0721386a9e8a949bc22ce13dabec18c8837
DESKTOP-E9ALBBV\profchydon <profchydon@gmail.com>
2017-08-28 16:33:37 +0100

    Test

--- temporary/test.php
+++ temporary/test.php
@@ -3,13 +3,13 @@
     // require 'Poloniex.php';
     //
     // $poloniex = new Poloniex;
-    //
-    // $homepage = $poloniex->curl_get_file_contents('http://poloniex.com/public?command=returnTicker');
-    //
-    // $homepage = file_get_contents('http://poloniex.com/public?command=returnTicker');
-    // echo $homepage;
 
-    echo "Test";
+    // $homepage = $poloniex->curl_get_file_contents('http://poloniex.com/public?command=returnTicker');
+
+    $homepage = file_get_contents('http://poloniex.com/public?command=returnTicker');
+    echo $homepage;
+
+    // echo "Test";
 
 
 ?>
