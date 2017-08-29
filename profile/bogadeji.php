f49a8244a042242abfe76fae4e1759eedebe20ac
Adeboga <bogadeji@gmail.com>
2017-08-30 00:50:29 +0100

    update

--- profile/bogadeji.php
+++ profile/bogadeji.php
@@ -1,7 +1,7 @@
 <?php
 // define variables and set to empty values
  
-    
+    if($_SERVER['REQUEST_METHOD'] == 'GET')  {
     $to = "bogadeji@gmail.com";
     $subject = $_GET['subject'];
     $body = $_GET['message']; 
@@ -18,9 +18,9 @@
            header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
       
         }else{
-          echo "Some fields are empty"
+          echo "Some fields are empty";
         }
-      
+      }
  //$url = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password&to=bogadeji@gmail.com";
 
 
