4af9f0383cda096c0f14696e36beaa2e9815c56e
kennylevi <kennylevi90@gmail.com>
2017-08-29 11:57:55 +0100

    my modifed participant

--- participants.php
+++ participants.php
@@ -8161,5 +8161,28 @@ include('participants-head.php');
 </div>
 <!-- Each participant ends here -->
 
+<!--  Each Participant starts here  -->
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(http://hng.fun/profile/kennylevi/facebook.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/kennylevi/kennylevi.html">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Kenny Levi Opara</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">kennylevi90@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> Team 5 </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+<!--  Each Participants ends here  -->
+
 
 <?php include "footer.php"?>
