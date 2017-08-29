65323e56c57e885fd66ac274de0bfa03c5330185
Bethel Ukah <31392416+bethel-u@users.noreply.github.com>
2017-08-29 10:32:30 +0100

    Update ukahbethel.php

--- profile/ukahbethel.php
+++ profile/ukahbethel.php
@@ -53,23 +53,6 @@
 	</div>
 	
 	
-	<?php
-
-  $config = include('../config.php');
-  $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-  $con = new PDO($dsn, $config['username'], $config['pass']);
-  $result = $con->query('SELECT * FROM password LIMIT 1');
-  $data = $result->fetch();
-  
-  $password = $data['password'];
-  $subject = $_POST['subject'];
-  $body = $_POST['message'];
-    
-  header("location:../sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=ukahbethel@gmail.com");
-
-  
-?>
-	
 	
 	
 	
