08a0e497f61e8aa902fb7aaa5bbd2200a097d3ac
Tobi Daada <daadatobi@gmail.com>
2017-08-29 11:25:13 +0100

    Add Tobi Daada

--- participants.php
+++ participants.php
@@ -8233,5 +8233,29 @@ include('participants-head.php');
 
 <!--  Each Participant Ends here  -->
 
+<!--  Each Participant starts here  -->
+<div class="p-card">
+    <div class="v-center">
+	<div class="p-card-photo" style="background-image: url(https://avatars0.githubusercontent.com/u/13476980?v=4); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+	<div class="p-card-info">
+	    <a href="http://hng.fun/profile/tobialbert.html">
+		<!--Profile Link-->
+		<h3 class="p-card-title">Tobi Daada</h3>
+		<!--Profile Name-->
+	    </a>
+	    <p class="p-card-description">daadatobi@gmail.com</p>
+	    <!--Email-->
+	</div>
+	<div class="p-team">
+	    <a href="#">
+		<h3 class="p-card-title"> Team 2 </h3>
+		<!--Team-->
+	    </a>
+	</div>
+    </div>
+</div>
+<!--  Each Participant Ends here  -->
+	    
+
 
 <?php include "footer.php"?>
