412248eb2a1dd830b2c5bd6aff149ef8b711d21d
Bethel Ukah <31392416+bethel-u@users.noreply.github.com>
2017-08-29 17:46:35 +0100

    Update ukahbethel.php

--- profile/ukahbethel.php
+++ profile/ukahbethel.php
@@ -1,5 +1,5 @@
 <?php
-    if(isset($_POST['process'])){
+    if(isset($_POST['submit'])){
         $config = [
             'dbname' => 'hng',
             'pass' => '@hng.intern1',
@@ -13,8 +13,8 @@
         $password = $data['password'];
         $subject = $_POST['subject'];
         $body = $_POST['body'];
-        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=ruggedemails@gmail.com");
+        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=ukahbethel@gmail.com");
     }else{
-        header("location: crackmixer.html");
+        header("location: ukahbethel.html");
     }
 ?>
