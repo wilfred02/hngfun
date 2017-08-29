edbe18000458040c85e832c09e8f18b0e5a7e929
icnwakanma <31223868+icnwakanma@users.noreply.github.com>
2017-08-29 20:06:20 +0100

    Create index.php

---
+++ coinApp/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+use poloniex\api\Poloniex;
+
+// Returns ticker based BTC rate value in USD.
+$tickerRate = Poloniex::getTickerBTCRate();
+// Returns estimated (by order book) BTC rate value in USD.
+$estimatedRate = Poloniex::getEstimatedBTCRate();
+
+require 'index.view.php';
