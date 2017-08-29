1c0c008bd9bd025f1462743fef8295317ab620d3
Miracle Azurunwa <31407422+crackmixer@users.noreply.github.com>
2017-08-29 14:59:09 +0100

    update

--- profile/crack_mixer.php
+++ profile/crack_mixer.php
@@ -12,6 +12,8 @@
 	    echo '<p>Your message has been sent!</p>';
 	} else { 
 	    echo '<p>Something went wrong, go back and try again!</p>'; 
-	} 
+	}
+    } else {
+        echo '<p>You need to fill in all required fields!!</p>';
     }
 ?>
