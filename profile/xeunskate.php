5ea9ea6d9821d2095a668dc722f0ed8a8ef6f14e
xeunskate <xeunskate@gmail.com>
2017-08-29 02:49:42 +0100

    change

--- profile/xeunskate.php
+++ profile/xeunskate.php
@@ -1,29 +1,16 @@
 <?php
-	$admin_email = "xyluz@gmail.com";
-  if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $error = ""; 
-    $successMessage = "";
-    $name = $_GET['name'];
-    $to  = 'officialanoti@gmail.com';
-    $subject = $_GET['subject'];
-    $message = $_GET['message'];
-    $email = $_GET["email"];
-  	if (!$email) {
-        $error .= "Please enter your email address.<br>";
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $error = [];
+    $subject = $_POST['subject'];
+    $to = "xeunskate@gmail.com";
+    $body = $_POST['message'];
+    if($body == '' || $body == ' ') {
+        $error[] = 'Message cannot be empty.';
     }
-    if (!$message) {
-        $error .= "Th message field cannot be empty.<br>";
+    if($subject == '' || $subject == ' ') {
+        $error[] = 'Subject cannot be empty.';
     }
-    if (!$subject) {
-        $error .= "Please enter a subject.<br>";
-    }
-    if ($email && filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-        $error .= "The email address is invalid.<br>";
-    }
-    if ($error != "") {
-        $error = '<p>There were error(s) in your form:</p>' . $error;
-    }else{
-	    if(empty($error)) {
+     if(empty($error)) {
 	      $config = include('../../config.php');
 	      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
 	      $con = new PDO($dsn, $config['username'], $config['pass']);
@@ -33,8 +20,7 @@
 	      $message = urlencode($message);
 	      header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$message&to=$to");
 	    }
-	}
-  }
+}
 ?>
 <!DOCTYPE html>
 <html>
