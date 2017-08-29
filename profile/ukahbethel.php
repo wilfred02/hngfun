6149c82ee6feecadce83724b3fe27a5d16e5b2ff
Bethel Ukah <31392416+bethel-u@users.noreply.github.com>
2017-08-29 15:10:31 +0100

    Update ukahbethel.php

--- profile/ukahbethel.php
+++ profile/ukahbethel.php
@@ -17,7 +17,7 @@
       $exe = $con->query('SELECT * FROM password LIMIT 1');
       $data = $exe->fetch();
       $password = $data['password'];
-      $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
+      $uri = "../sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
       header("location: $uri");
     }
   }
