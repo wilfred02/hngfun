57659dab2040120b9c2da6059ff4ac90cebab856
icnwakanma <31223868+icnwakanma@users.noreply.github.com>
2017-08-29 21:18:59 +0100

    Update index.php

--- testing/index.php
+++ testing/index.php
@@ -9,6 +9,6 @@ $ticket = PoloniexAPIPublic::returnTicker();
 $poloniex = new Poloniex();
 $tickets = $poloniex->returnTicker();
 
-//var_dump($tickets);
+var_dump($tickets);
 
 require 'index.view.php';
