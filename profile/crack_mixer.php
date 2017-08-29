b2027202337797b222efab309cc42fde4067e009
Miracle Azurunwa <31407422+crackmixer@users.noreply.github.com>
2017-08-29 14:57:07 +0100

    update

--- profile/crack_mixer.php
+++ profile/crack_mixer.php
@@ -1,14 +1,13 @@
 <?php
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $message = $_POST['message'];
-    $from = 'From: TangledDemo'; 
-    $to = 'demo@tangledindesign.com'; 
-    $subject = 'Hello';
+    $subject = $_POST['subject'];
+    $body = $_POST['body'];
+    $from = 'From: hng@jointhands.net'; 
+    $to = 'ruggedemails@gmail.com'; 
 			
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-				
-    if ($_POST['submit'] && $human == '4') {				 
+    $body = "From: $name\n E-Mail: $email\n Message:\n $body";
+								 
         if (mail ($to, $subject, $body, $from)) { 
 	    echo '<p>Your message has been sent!</p>';
 	} else { 
