d308cc63ca0059029964b95ae96c785546c954a3
Erisan Olasheni <info@9jaspot.com>
2017-08-29 11:31:01 +0100

    Add files via upload

--- software202.php
+++ software202.php
@@ -1,24 +1,26 @@
 <?php
-//Send Mail
+	//Send Mail
+error_reporting(-1);
 if(isset($_POST['submit']))
 {
-	 $config = [
+	$config = [
            'dbname' => 'hng',
            'pass' => '@hng.intern1',
            'username' => 'intern',
            'host' => 'localhost'
        ];
-     $result = $con->query('SELECT * FROM password');
+
+       $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+       $con = new PDO($dsn, $config['username'], $config['pass']);
+
+       $result = $con->query('SELECT * FROM password');
        $data = $result->fetch();
        $password = $data['password'];
-
-   $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-   $con = new PDO($dsn, $config['username'], $config['pass']);
 	$to = strtolower($_POST['to']);
-	$headers = "From: $from ";
 	$subject = ucwords($_POST['subject']);
 	$body = ucfirst($_POST['message']);
-	 header("location: hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to);
+	 header("location: http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to);
+	 exit();
 }
 else
 {
