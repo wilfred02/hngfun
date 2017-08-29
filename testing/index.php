f9bea1fe8a43d47e42c0c5e3ac4ca45eb60a5235
icnwakanma <31223868+icnwakanma@users.noreply.github.com>
2017-08-29 20:50:48 +0100

    Update index.php

--- testing/index.php
+++ testing/index.php
@@ -7,7 +7,7 @@ use poloniex\api\Poloniex;
 $ticket = PoloniexAPIPublic::returnTicker();
 // Dynamic call
 $poloniex = new Poloniex();
-$tickets = $poloniex->returnTicker();
+$tickets = $poloniex->returnCurrencies();
 
 //var_dump($tickets);
 
