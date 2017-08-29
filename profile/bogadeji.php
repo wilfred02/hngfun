88c3ea9c8cd312c88f69be46dde139e6307dc0ea
Adeboga <bogadeji@gmail.com>
2017-08-29 23:51:46 +0100

    update

--- profile/bogadeji.php
+++ profile/bogadeji.php
@@ -7,16 +7,16 @@
      // if (isset($_GET['submit']))  {
           //Email information here
       $to = "bogadeji@gmail.com";
-     // $subject = $_GET['subject'];
-     // $body = $_GET['message'];    
+      $subject = $_GET['subject'];
+     $body = $_GET['message'];    
       $config = include(dirname(dirname(__FILE__)).'/config.php');
       $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
       $con = new PDO($dsn, $config['username'], $config['pass']);
       $exe = $con->query('SELECT * FROM password LIMIT 1');
       $data = $exe->fetch();
       $password = $data['password'];
-           // header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
-      echo $data;
+           header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
+      
        // }
 
  //$url = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password&to=bogadeji@gmail.com";
