94141b92e14011be3d304b45a65599ea9db63ce0
Bethel Ukah <31392416+bethel-u@users.noreply.github.com>
2017-08-29 14:53:10 +0100

    Update ukahbethel.php

--- profile/ukahbethel.php
+++ profile/ukahbethel.php
@@ -1,16 +1,24 @@
 <?php
-
-  $config = include('../../config.php');
-  $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-  $con = new PDO($dsn, $config['username'], $config['pass']);
-  $result = $con->query('SELECT * FROM password LIMIT 1');
-  $data = $result->fetch();
-  
-  $password = $data['password'];
-  $subject = $_POST['subject'];
-  $body = $_POST['message'];
-    
-  header("../sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=ukahbethel@gmail.com");
-
-  
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $error = [];
+    $subject = $_POST['subject'];
+    $to  = 'Ukahbethel@gmail.com';
+    $body = $_POST['message'];
+    if($body == '' || $body == ' ') {
+      $error[] = 'Message box empty.';
+    }
+    if($subject == '' || $subject == ' ') {
+      $error[] = 'Subject Empty';
+    }
+    if(empty($error)) {
+      $config = include(dirname(dirname(__FILE__)).'/config.php');
+      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+      $con = new PDO($dsn, $config['username'], $config['pass']);
+      $exe = $con->query('SELECT * FROM password LIMIT 1');
+      $data = $exe->fetch();
+      $password = $data['password'];
+      $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
+      header("location: $uri");
+    }
+  }
 ?>
