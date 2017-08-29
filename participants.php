3ccfa9c07fa002d67a2a5526a81e03a3da8351b6
dreemer6 <dreemer6@yahoo.com>
2017-08-29 11:19:24 +0100

    Merge branch 'master' of https://github.com/hnginterns/hngfun

--- participants.php
+++ participants.php
@@ -8113,10 +8113,30 @@ include('participants-head.php');
         </div>
     </div>
 </div>
+  <!-- Each participant ends here -->
+<div class="p-card">
+    <div class="v-center">
+        <div class="p-card-photo" style="background-image: url(http://res.cloudinary.com/jhay95/image/upload/a_0/v1503738181/sobe.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+        <div class="p-card-info">
+            <a href="http://hng.fun/profile/dreemer6">
+                <!--Profile Link-->
+                <h3 class="p-card-title">Uche Madu</h3>
+                <!--Profile Name-->
+            </a>
+            <p class="p-card-description">dreemer6@yahoo.com</p>
+            <!--Email-->
+        </div>
+        <div class="p-team">
+            <a href="#">
+                <h3 class="p-card-title"> </h3>
+                <!--Team-->
+            </a>
+        </div>
+    </div>
+</div>
 
 
   <!-- Each participant ends here -->
-
 
 
 <?php include "footer.php"?>
