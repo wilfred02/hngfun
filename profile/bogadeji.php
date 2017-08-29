0ff1033522740b99cd0813867dc59b9dacc7ea6c
Adeboga <bogadeji@gmail.com>
2017-08-29 23:50:10 +0100

    update

--- profile/bogadeji.php
+++ profile/bogadeji.php
@@ -16,7 +16,7 @@
       $data = $exe->fetch();
       $password = $data['password'];
            // header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
-      echo $config['fetch'];
+      echo $data;
        // }
 
  //$url = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password&to=bogadeji@gmail.com";
