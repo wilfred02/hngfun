7f91043119025fb9e7adad7bcbe342c4fba383f1
icnwakanma <31223868+icnwakanma@users.noreply.github.com>
2017-08-29 20:31:18 +0100

    Update index.php

--- testing/index.php
+++ testing/index.php
@@ -4,9 +4,9 @@ require_once("vendor/autoload.php");
 use poloniex\api\PoloniexAPIPublic;
 use poloniex\api\Poloniex;
 // Static call
-$tickets = PoloniexAPIPublic::returnTicker();
+$ticket = PoloniexAPIPublic::returnTicker();
 // Dynamic call
 $poloniex = new Poloniex();
-$ticket = $poloniex->returnTicker();
+$tickets = $poloniex->returnTicker();
 
 require 'index.view.php';
