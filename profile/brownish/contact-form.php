aa345f1c79c0da51736361f880bfbf40824129f1
brown-son <brown-son@users.noreply.github.com>
2017-08-30 21:57:25 +0100

    Stripping Redundant Codes

--- profile/brownish/contact-form.php
+++ profile/brownish/contact-form.php
@@ -1,14 +1,10 @@
 <?php
    if(isset($_POST['process'])){
 
-      $config = [
-           'dbname' => 'hng',
-           'pass' => '@hng.intern1',
-           'username' => 'intern',
-           'host' => 'localhost'
-       ];
 
-      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+       $config = include('../../config.php');
+
+       $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
        $con = new PDO($dsn, $config['username'], $config['pass']);
 
       $result = $con->query('SELECT * FROM password');
