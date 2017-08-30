e5dda664caffaf9114475d06cda32f4106ea653c
brown-son <brown-son@users.noreply.github.com>
2017-08-30 10:17:56 +0100

    Brownish Profile Entry

--- participants.php
+++ participants.php
@@ -7376,9 +7376,29 @@ include('participants-head.php');
                     </div>
                 </div>
             </div>
-
-
-
+<!--  Each Participant Ends here  -->
+<!--  Each Participant starts here  -->
+<div class="p-card">
+	<div class="v-center">
+	<div class="p-card-photo" style="background-image: url(https://avatars0.githubusercontent.com/u/13476980?v=4); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+	<div class="p-card-info">
+	    <a href="http://hng.fun/brownish/index.html">
+		<!--Profile Link-->
+		<h3 class="p-card-title">Ekebo Brown Etuk</h3>
+		<!--Profile Name-->
+	    </a>
+	    <p class="p-card-description">ekebo.etuk@gmail.com</p>
+	    <!--Email-->
+	</div>
+	<div class="p-team">
+	    <a href="#">
+		<h3 class="p-card-title"> None </h3>
+		<!--Team-->
+	    </a>
+	</div>
+    </div>
+</div>
+<!--  Each Participant Ends here  -->
 		    <!-- Each participant starts here -->
             <div class="p-card">
                 <div class="v-center">
@@ -8414,29 +8434,6 @@ include('participants-head.php');
      </div> 
  </div> 
  <!--  Each Participants ends here  --> 
- <!--  Each Participant starts here  -->
-<div class="p-card">
-
-	<div class="v-center">
-	<div class="p-card-photo" style="background-image: url(https://avatars0.githubusercontent.com/u/13476980?v=4); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
-	<div class="p-card-info">
-	    <a href="http://hng.fun/brownish/index.html">
-		<!--Profile Link-->
-		<h3 class="p-card-title">Ekebo Brown Etuk</h3>
-		<!--Profile Name-->
-	    </a>
-	    <p class="p-card-description">ekebo.etuk@gmail.com</p>
-	    <!--Email-->
-	</div>
-	<div class="p-team">
-	    <a href="#">
-		<h3 class="p-card-title"> None </h3>
-		<!--Team-->
-	    </a>
-	</div>
-    </div>
-</div>
-<!--  Each Participant Ends here  -->
 
 <!--  Each Participant starts here  -->
 <div class="p-card">
