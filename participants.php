9ad8d6035c6328217252e18051ae47258df30d55
franklyn034 <franklynugege@gmail.com>
2017-08-30 04:58:51 +0100

    Modified file location frankly034

--- participants.php
+++ participants.php
@@ -1,4 +1,4 @@
-﻿hp
+﻿<?php
 $pageTitle = "Participants";
 include('header.php');
 include('participants-head.php');
@@ -8439,5 +8439,27 @@ include('participants-head.php');
 </div>
 <!--  Each Participant Ends here  -->
 
+<!-- Each participant starts here -->
+  <div class="p-card">
+    <div class="v-center">
+        <div class="p-card-photo" style="background-image: url(http://res.cloudinary.com/dmyow6n0v/image/upload/v1503540677/IMG_20160424_112337_edit_vz0vwz.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+        <div class="p-card-info">
+            <a href="http://hng.fun/profile/frankly034.php">
+                <!--Profile Link-->
+                <h3 class="p-card-title">Franklyn Ugege</h3>
+                <!--Profile Name-->
+            </a>
+            <p class="p-card-description">franklynugege@gmail.com</p>
+            <!--Email-->
+        </div>
+        <div class="p-team">
+            <a href="#">
+                <h3 class="p-card-title"></h3>
+                <!--Team-->
+            </a>
+        </div>
+    </div>
+</div>
+<!-- Each participant ends here -->
 
 <?php include "footer.php"?>
