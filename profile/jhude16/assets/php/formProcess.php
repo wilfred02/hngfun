ca4a95fc7169c4cc1e2052499a78bf802cb29968
Perfect Gentleman <judehawkson@yahoo.com>
2017-08-30 12:38:16 +0100

    Update formProcess.php

--- profile/jhude16/assets/php/formProcess.php
+++ profile/jhude16/assets/php/formProcess.php
@@ -27,8 +27,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $result = $con->query('SELECT * FROM password LIMIT 1');
         $data = $result->fetch();
         $password = $data['password'];
-        $url = "http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$message&to=judehawkson@yahoo.com";
-//         $url = "http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to;
+//         $url = "http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$message&to=judehawkson@yahoo.com";
+        $url = "http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$name." ".$message."&to=".$to;
         header("location: ".$url);
       
     } else {
