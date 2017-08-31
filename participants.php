d6a961831bfdfa6310a85f37d44de1cafb86017a
MercyIkpe <ikpemercy1@gmail.com>
2017-08-31 13:54:39 +0100

    updated.
    changes made to profile and participant page.

--- participants.php
+++ participants.php
@@ -22,6 +22,28 @@ include('participants-head.php');
               - Pull the repo, before pushing it, inorder to prevent conflicts
             -->
 
+<!--  Each Participant starts here  -->
+<div class="p-card">
+    <div class="v-center">
+        <div class="p-card-photo" style="background-image: url(http://res.cloudinary.com/mercyikpe/image/upload/a_hflip/v1503966085/mercyikpe_x6pkwm.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+        <div class="p-card-info">
+            <a href="http://hng.fun/profile/mercyikpe/index.html">
+                <!--Profile Link-->
+                <h3 class="p-card-title">Mercy Ikpe</h3>
+                <!--Profile Name-->
+            </a>
+            <p class="p-card-description">ikpemercy1@gmail.com</p>
+            <!--Email-->
+        </div>
+        <div class="p-team">
+            <a href="#">
+                <h3 class="p-card-title"> Team-Achievement </h3>
+                <!--Team-->
+            </a>
+        </div>
+    </div>
+</div>
+
 
 <!--  Each Participant starts here  -->
 <div class="p-card">
