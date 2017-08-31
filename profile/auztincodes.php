fb84466a21d0f0d3800b4c02ce5abac228f93989
Okorie Augustine <austinugo200@gmail.com>
2017-08-31 01:09:39 +0100

    Update auztincodes.php

--- profile/auztincodes.php
+++ profile/auztincodes.php
@@ -22,9 +22,7 @@
 	     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 		<style type="text/css">
-			html{
-				height:100%;
-			}
+			
 			body{
 			 	background:#fff;
 			 	background-image: url("http://res.cloudinary.com/auztin/image/upload/q_100/v1503444337/bg2_xu9ggq.jpg");
