08db537593d705bb8b87a0694e9c85a74c20d2aa
Maxwell444 <spencemaxwell4@gmail.com>
2017-08-31 11:53:59 +0100

    name added to particpants

--- participants.php
+++ participants.php
@@ -8593,5 +8593,26 @@ include('participants-head.php');
     </div>
 </div>
 
+<div class="p-card">
+    <div class="v-center">
+        <div class="p-card-photo" style="background-image: url(https://avatars0.githubusercontent.com/u/26363001?v=4&s=460); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+        <div class="p-card-info">
+            <a href="http://hng.fun/profile/maxwell4reel.php">
+                <!--Profile Link-->
+                <h3 class="p-card-title">Nwafor Maxwell Chukwebuka</h3>
+                <!--Profile Name-->
+            </a>
+            <p class="p-card-description">spencemaxwell4@gmail.com</p>
+            <!--Email-->
+        </div>
+        <div class="p-team">
+            <a href="#">
+                <h3 class="p-card-title"> No Team </h3>
+                <!--Team-->
+            </a>
+        </div>
+    </div>
+</div>
+
 
 <?php include "footer.php"?>
