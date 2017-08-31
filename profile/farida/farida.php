f4808cd8ebc0b675b65a70d2c313f7c9a384574f
Farida Kabir <fareedakabeer@gmail.com>
2017-09-01 00:40:09 +0100

    farida

--- profile/farida/farida.php
+++ profile/farida/farida.php
@@ -1,30 +1,38 @@
 <?php
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$subject = $_POST['subject'];
-	$body = $_POST['message'];
+ $name = $_POST['name'];
+ $email = $_POST['email'];
+ $subject = $_POST['subject'];
+ $body = $_POST['message'];
 
 
-	$to  = 'fareedakabeer@gmail.com';
-	$admin_email = 'xyluz@ymail.com';
-	$error = "";
+$to  = 'fareedakabeer@gmail.com';
+$admin_email = 'xyluz@ymail.com';
+$error = "";
 
-	if (!$email) {
-		$error .= "Your email address is required.<br>";
-	}
+if (!$email) {
+	$error .= "Your email address is required.<br>";
+}
 
-	if (!$subject) {
-		$error .= "A subject is required.<br>";
-	}
+if (!$subject) {
+	$error .= "A subject is required.<br>";
+}
 
-	if (!$body) {
-		$error .= "Your message is required.<br>";
-	}
+if (!$body) {
+	$error .= "Your message is required.<br>";
+}
 
+if ($email && filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+        $error .= "The email address is invalid.<br>";
+    }
+    if ($error != "") {
+        $error = '<p>There were error(s) in your form:</p>' . $error;
+}
+
+else{
 	if(empty($error)) {
-		$config = include('../../config.php');
+		$config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
 		$dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
 		$con = new PDO($dsn, $config['username'], $config['pass']);
 		
@@ -32,10 +40,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$data = $exe->fetch();
 		$password = $data['password'];
 
-		header("location:http://hng.fun/sendmail.php?password=".$password."&name=".$name."&email=".$email."&subject=".$subject."&message=".$body."&to=fareedakabeer@gmail.com");
+		       header("location:http://hng.fun/sendmail.php?password=".$password."&name=".$name."&email=".$email."&subject=".$subject."&message=".$body."&to=fareedakabeer@gmail.com");
 	}
-	
-}
+			 
+	}
 }
 ?>
 
