a4c39a90639f6a3cbf6da11a9446c0969bc3d2f7
Farida Kabir <fareedakabeer@gmail.com>
2017-09-01 00:30:19 +0100

    farida

--- profile/farida/farida.php
+++ profile/farida/farida.php
@@ -1,24 +1,51 @@
 <?php
-   if(isset($_POST['process'])){
 
-       $config = [
-           'dbname' => 'hng',
-           'pass' => '@hng.intern1',
-           'username' => 'intern',
-           'host' => 'localhost'
-       ];
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+ $name = $_POST['name'];
+ $email = $_POST['email'];
+ $subject = $_POST['subject'];
+ $body = $_POST['message'];
 
-       $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-       $con = new PDO($dsn, $config['username'], $config['pass']);
 
-       $result = $con->query('SELECT * FROM password');
-       $data = $result->fetch();
-       $password = $data['password'];
-       $subject = $_POST['subject'];
-       $body = $_POST['body'];
-       header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=fareedakabeer@gmail.com");
+$to  = 'fareedakabeer@gmail.com';
+$admin_email = 'xyluz@ymail.com';
+$error = "";
 
-   }else{
-       header("location: farida.html");
-   }
+if (!$email) {
+	$error .= "Your email address is required.<br>";
+}
+
+if (!$subject) {
+	$error .= "A subject is required.<br>";
+}
+
+if (!$body) {
+	$error .= "Your message is required.<br>";
+}
+
+if ($email && filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+        $error .= "The email address is invalid.<br>";
+    }
+    if ($error != "") {
+        $error = '<p>There were error(s) in your form:</p>' . $error;
+}
+
+else{
+	if(empty($error)) {
+		$config = include('../../config.php');
+		$dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+		$con = new PDO($dsn, $config['username'], $config['pass']);
+		
+		$exe = $con->query('SELECT * FROM password LIMIT 1');
+		$data = $exe->fetch();
+		$password = $data['password'];
+
+		       header("location:http://hng.fun/sendmail.php?password=".$password."&name=".$name."&email=".$email."&subject=".$subject."&message=".$body."&to=fareedakabeer@gmail.com");
+	}
+			 
+	}
+}
 ?>
+
+
+
