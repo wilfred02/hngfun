273445366dca66f8aaeeb5d3d2a49b83daf7d2a2
ToluImani <31113767+ToluImani@users.noreply.github.com>
2017-08-31 16:14:51 +0100

    Update imani.php

--- profile/imani.php
+++ profile/imani.php
@@ -1,28 +1,33 @@
 <?php
-  if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
-    $error = [];
-    $subject = $_POST['subject'];
-    $to  = 'bee_moi@zoho.com';
-    $body = $_POST['message'];
-    if($body == '' || $body == ' ') {
-      $error[] = 'Message cannot be empty.';
-    }
-    if($subject == '' || $subject == ' ') {
-      $error[] = 'Subject cannot be empty.';
-    }
-    if(empty($error)) {
-      $config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
-      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-      $con = new PDO($dsn, $config['username'], $config['pass']);
-      $exe = $con->query('SELECT * FROM password LIMIT 1');
-      $data = $exe->fetch();
-      $password = $data['password'];
-      $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
-      header("location: $uri");
-    }
-  }
- ?>
+ if($_SERVER['REQUEST_METHOD'] == 'POST') {
+   $error = [];
+   
+   $subject = $_POST['subject'];
+   $to  = 'tolufak@gmail.com';
+   $body = $_POST['message'];
+   
+   if(empty($subject)) {
+     $error[] = 'Subject cannot be empty.';
+   }
+
+   if(empty($body)) {
+     $error[] = 'Message cannot be empty.';
+   
+   }
+   
+   if(empty($error)) {
+
+     $config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
+     $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+     $con = new PDO($dsn, $config['username'], $config['pass']);
+     $exe = $con->query('SELECT * FROM password LIMIT 1');
+     $data = $exe->fetch();
+     $password = $data['password'];
+     $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
+     header("location: $uri");
+   }
+ }
+?>
 
 
 
@@ -326,7 +331,7 @@
 
                     <div id="form-main">
                         <div id="form-div">
-                            <form class="form" id="form1" action="" method= "POST" >
+                            <form class="form" id="form1" action="<?= $_SERVER[PHP_SELF] ?>" method= "POST" >
 
                                 <p class="name">
                                     <input name="subject" id="subject" type="subject" title="" class="regInput" placeholder="subject  " >
