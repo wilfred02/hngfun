4782ff770788dd860dab36a4ea8074a042891938
Maxwell444 <spencemaxwell4@gmail.com>
2017-08-31 12:07:51 +0100

    updated

--- profile/maxwell4reel.php
+++ profile/maxwell4reel.php
@@ -145,7 +145,7 @@
 	<div class="bg">
 		<div class="text">
 			<h1>Nwafor Maxwell Chukwuebuka</h1>
-			<p><a style="color: white;" href="https://github.com/maxwell4reel"><i class="fa fa-github">Username: Maxwell4reel</i></a></p><br>
+			<p class="tex"><a href="https://github.com/maxwell4reel"><i class="fa fa-github">Username: Maxwell4reel</i></a></p>
 			<p><a style="color: white;" href="https://github.com/maxwell4reel/Hngtest_register"><i class="fa fa-github"></i> Click here to view my Stage 1 Project</a></p>
 			<p class="tex"><a href="hnginterns.slack.com/team/maxwell444"><i class="fa fa-slack">Slack:</i> Maxwell444</a></p>
 			<div class="bio">
