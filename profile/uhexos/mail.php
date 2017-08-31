b39a0762ca3b0c2ee2f403c1ad542cce083c5209
Uhexos <uhexos@gmail.com>
2017-08-31 18:32:12 +0000

    tweak

--- profile/uhexos/mail.php
+++ profile/uhexos/mail.php
@@ -3,7 +3,7 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "uhexos@gmail.com";
-$subject = $_POSt["subject"];
+$subject = $_POST["subject"];
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
