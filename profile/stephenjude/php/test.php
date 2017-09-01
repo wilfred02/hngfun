15ba1e33932e9982c160b636b713efad91cedad1
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-01 22:45:23 +0100

    Update test.php

--- profile/stephenjude/php/test.php
+++ profile/stephenjude/php/test.php
@@ -6,8 +6,8 @@ $api_key = "52P8WQDT-UNTGQINY-A7RUEC8A-PJ7RQCKL"; // Api Keys here
 $api_secret = "1d63d60d45b510553c0bc7dd2aa4128e8b36c44d23a47459e03ef077bb5c1d25a098e1139434c7ceb97ed3ab7673164a6f12a67432d5062be08992b5c04e358e"; // Api Secret here
 
 # loadclass
-echo $load_trade = new poloniex($api_key, $api_secret);
-echo $trade_ticker = $load_trade->get_trading_pairs();
+$load_trade = new poloniex($api_key, $api_secret);
+$trade_ticker = $load_trade->get_trading_pairs();
 
 ?>
 					
@@ -17,7 +17,7 @@ echo $trade_ticker = $load_trade->get_trading_pairs();
 		<body>
 					
 				<?php	
-			
+				echo $trade_ticker;
 				?>
 		</body>
 		</html>
