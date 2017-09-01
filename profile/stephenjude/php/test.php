0d9ab204732ee335fef948dba39f1fde74801e74
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-01 22:27:12 +0100

    testing

--- profile/stephenjude/php/test.php
+++ profile/stephenjude/php/test.php
@@ -1,4 +1,5 @@
 
+<?php
 require ("p_api.php");
   
 $api_key = "52P8WQDT-UNTGQINY-A7RUEC8A-PJ7RQCKL"; // Api Keys here
@@ -8,7 +9,6 @@ $api_secret = "1d63d60d45b510553c0bc7dd2aa4128e8b36c44d23a47459e03ef077bb5c1d25a
 $load_trade = new poloniex($api_key, $api_secret);
 $trade_ticker = $load_trade->get_trading_pairs();
 
-<?php
 
 					foreach ($load_ticker as $key => $arrays) {	
 						echo '<tr>';
