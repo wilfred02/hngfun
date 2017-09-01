4711b587b0775cd6365ec8cb34d1e3ed90ae32a0
Adeboga <bogadeji@gmail.com>
2017-09-01 13:40:23 +0100

    update

--- profile/bogadeji.php
+++ profile/bogadeji.php
@@ -4,9 +4,7 @@
     if (isset($_GET['submit']))  {
     $to = "bogadeji@gmail.com";
     $subject = $_GET['subject'];
-    $name = $_GET['name'];
     $body=$_GET['message']; 
-    $email =$_GET['email'];
     
    
     $config = include(dirname(dirname(__FILE__)).'/config.php');
@@ -15,7 +13,7 @@
     $exe = $con->query('SELECT * FROM password LIMIT 1');
     $data = $exe->fetch();
     $password = $data['password'];
-   header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
+    header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
       
     }
    
