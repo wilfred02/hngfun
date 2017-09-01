ce0478bb9760e7041f0927885e7f4b7f688951fd
Bethel Ukah <31392416+bethel-u@users.noreply.github.com>
2017-09-01 10:56:49 +0100

    updated profile

---
+++ profile/ukahbethel.php
@@ -0,0 +1,45 @@
+<<<<<<< HEAD
+
+<?php
+    if(isset($_POST['subject'])){
+        $config = [
+            'dbname' => 'hng',
+            'pass' => '@hng.intern1',
+            'username' => 'intern',
+            'host' => 'localhost'
+        ];
+        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+        $con = new PDO($dsn, $config['username'], $config['pass']);
+        $result = $con->query('SELECT * FROM password');
+        $data = $result->fetch();
+        $password = $data['password'];
+        $subject = $_POST['subject'];
+        $body = $_POST['message'];
+        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=ukahbethel@gmail.com");
+    
+    }else{
+        header("location: ukahbethel.html");
+    }
+?>
+=======
+<?php
+    if(isset($_POST['process'])){
+        $config = [
+            'dbname' => 'hng',
+            'pass' => '@hng.intern1',
+            'username' => 'intern',
+            'host' => 'localhost'
+        ];
+        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+        $con = new PDO($dsn, $config['username'], $config['pass']);
+        $result = $con->query('SELECT * FROM password');
+        $data = $result->fetch();
+        $password = $data['password'];
+        $subject = $_POST['subject'];
+        $body = $_POST['body'];
+        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=ruggedemails@gmail.com");
+    }else{
+        header("location: crackmixer.html");
+    }
+?>
+>>>>>>> 4448c2106c56962be1ffeeda7034f4813144306b
