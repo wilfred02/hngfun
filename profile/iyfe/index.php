e71b595c06586163c91a709d7ff664e7b40973ba
Ifunanya Ukachukwu <chijioke@Beautiful.local>
2017-09-01 10:58:55 -0400

    remove photo file

--- profile/iyfe/index.php
+++ profile/iyfe/index.php
@@ -1,5 +1,5 @@
 <?php
-   	$admin_email = "xyluz@gmail.com";
+   	$admin_email = "xyluz@ymail.com";
   if($_SERVER['REQUEST_METHOD'] == 'GET') {
     $error = ""; 
     $successMessage = "";
@@ -49,7 +49,7 @@
 	<div id = "container_2">
 		<h1>Ifunanya's Profile</h1>
 		<div class="left-half">
-				<img id = "dp" alt="Display Picture" src="iyfe.png">
+				<img id = "dp" alt="Display Picture" src="https://drive.google.com/file/d/0B4e4ri7wvyyAcVFxVmJFcG1CdTQ/view?usp=sharing">
 				<p> Hello, my name is Ifunanya Ukachukwu. I am learning to become a software developer. Before now, my focus has been on HTML, CSS and JavaScript. I am really excited about this internship.  If you like to connect please say hi.
 
 				</p>
