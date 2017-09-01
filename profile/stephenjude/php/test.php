64a73866297c552f2809e98387db49fedbb885cc
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-01 22:48:19 +0100

    Update test.php

--- profile/stephenjude/php/test.php
+++ profile/stephenjude/php/test.php
@@ -18,6 +18,21 @@ $trade_ticker = $load_trade->get_trading_pairs();
 					
 				<?php	
 				echo $trade_ticker;
+				foreach ($trade_ticker as $key => $arrays) {	
+						echo '<tr>';
+						echo '<td><a href="trade_history.php?pair='.$key.'">'.$key.'</a></td>';
+						echo '<td>'.$arrays['id'].'</td>';
+						echo '<td>'.$arrays['last'].'</td>';
+						echo '<td>'.$arrays['lowestAsk'].'</td>';
+						echo '<td>'.$arrays['highestBid'].'</td>';
+						echo '<td>'.$arrays['percentChange'].'</td>';
+						echo '<td>'.$arrays['baseVolume'].'</td>';
+						echo '<td>'.$arrays['quoteVolume'].'</td>';
+						echo '<td>'.$arrays['isFrozen'].'</td>';
+						echo '<td>'.$arrays['high24hr'].'</td>';
+						echo '<td>'.$arrays['low24hr'].'</td>';
+						echo '</tr>';
+					}
 				?>
 		</body>
 		</html>
