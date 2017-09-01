f571d615a8d60a428b6134097e99fae32a9fc512
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-01 22:35:52 +0100

    test

--- profile/stephenjude/php/test.php
+++ profile/stephenjude/php/test.php
@@ -6,26 +6,31 @@ $api_key = "52P8WQDT-UNTGQINY-A7RUEC8A-PJ7RQCKL"; // Api Keys here
 $api_secret = "1d63d60d45b510553c0bc7dd2aa4128e8b36c44d23a47459e03ef077bb5c1d25a098e1139434c7ceb97ed3ab7673164a6f12a67432d5062be08992b5c04e358e"; // Api Secret here
 
 # loadclass
-$load_trade = new poloniex($api_key, $api_secret);
-$trade_ticker = $load_trade->get_trading_pairs();
+echo $load_trade = new poloniex($api_key, $api_secret);
+echo $trade_ticker = $load_trade->get_trading_pairs();
 
 
 					
-	
+	<html>
+		<head>
+		</head>
+		<body>
 					
 					foreach ($load_ticker as $key => $arrays) {	
-						echo '<tr>';
-						echo '<td><a href="trade_history.php?pair='.$key.'">'.$key.'</a></td>';
-						echo '<td>'.$arrays['id'].'</td>';
-						echo '<td>'.$arrays['last'].'</td>';
-						echo '<td>'.$arrays['lowestAsk'].'</td>';
-						echo '<td>'.$arrays['highestBid'].'</td>';
-						echo '<td>'.$arrays['percentChange'].'</td>';
-						echo '<td>'.$arrays['baseVolume'].'</td>';
-						echo '<td>'.$arrays['quoteVolume'].'</td>';
-						echo '<td>'.$arrays['isFrozen'].'</td>';
-						echo '<td>'.$arrays['high24hr'].'</td>';
-						echo '<td>'.$arrays['low24hr'].'</td>';
-						echo '</tr>';
+						echo '<br>';
+						echo '<br><a href="trade_history.php?pair='.$key.'">'.$key.'</a></td>';
+						echo '<br>'.$arrays['id'];
+						echo '<br>'.$arrays['last'];
+						echo '<br>'.$arrays['lowestAsk'];
+						echo '<br>'.$arrays['highestBid'];
+						echo '<br>'.$arrays['percentChange'];
+						echo '<br>'.$arrays['baseVolume'];
+						echo '<br>'.$arrays['quoteVolume'];
+						echo '<br>'.$arrays['isFrozen'];
+						echo '<br>'.$arrays['high24hr'];
+						echo '<br>'.$arrays['low24hr'];
+						echo '<br>';
 					}	
 				?>
+		</body>
+		</html>
