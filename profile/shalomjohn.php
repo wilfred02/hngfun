c922d500e5faa893c3a50f4a2bc665b64e35271a
otuekongjohn <johnprince58@gmail.com>
2017-09-02 03:34:06 +0100

    added

--- profile/shalomjohn.php
+++ profile/shalomjohn.php
@@ -1,37 +1,28 @@
 <?php
-  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $error = [];
-
     $subject = $_POST['subject'];
     $to  = 'shaliz.jhay@gmail.com';
     $body = $_POST['body'];
-
     if($body == '' || $body == ' ') {
-      $error[] = "Don't be shy. Write me a message";
+        $error[] = 'Message cannot be empty.';
     }
-
 
     if($subject == '' || $subject == ' ') {
-      $error[] = 'A subject here would be nice.';
+        $error[] = 'Subject cannot be empty.';
     }
-
     if(empty($error)) {
-
-      $config = include __DIR__ . "/../config.php";
-      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-      $con = new PDO($dsn, $config['username'], $config['pass']);
-
-      $exe = $con->query('SELECT * FROM password LIMIT 1');
-      $data = $exe->fetch();
-      $password = $data['password'];
-
-      $url = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
-
-      header("location: $url");
-
+        $config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
+        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+        $con = new PDO($dsn, $config['username'], $config['pass']);
+        $exe = $con->query('SELECT * FROM password LIMIT 1');
+        $data = $exe->fetch();
+        $password = $data['password'];
+        $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
+        header("location: $uri");
     }
-  }
- ?>
+}
+?>
 
 <!DOCTYPE html>
 <html>
