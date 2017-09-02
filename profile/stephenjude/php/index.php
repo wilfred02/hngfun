169ceaf30e91caeed2d41aacf24afd9d040aa60d
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-02 02:38:24 +0100

    Update index.php

--- profile/stephenjude/php/index.php
+++ profile/stephenjude/php/index.php
@@ -39,29 +39,10 @@
 		<p>This strategy show transaction gap between each crypto-currencies, buy transaction rate, the sell transaction rate and trade activity details. </p>
 		<hr />
 
-		<div id="buy-sell"></div>
-
-		<ul class="nav nav-tabs">
-			<li class="active"><a data-toggle="tab" href="#home">Current Updates </a></li>
-			<li><a data-toggle="tab" href="#menu1">Last 1 min of trade</a></li>
-			<li><a data-toggle="tab" href="#menu2">Last 3 mins of trade</a></li>
-			<li><a data-toggle="tab" href="#menu3">Last 24 mins of trade</a></li>
-		</ul>
-
-		<div class="tab-content">
-			<div id="home" class="tab-pane fade in active">
-				<div id="load-all"></div>
-			</div>
-			<div id="menu1" class="tab-pane fade">
-				<div id="load-all-1hr"></div>
-			</div>
-			<div id="menu2" class="tab-pane fade">
-				<div id="load-all-3hr"></div>
-			</div>
-			<div id="menu3" class="tab-pane fade">
-				<div id="load-all-24hr"></div>
-			</div>
-		</div>
+	<div id="home" class="tab-pane fade in active">
+		<div id="load-all"></div>
+	</div>
+			
 	</div>
 
 	<div class="container">
@@ -69,7 +50,7 @@
 			<div class="col-md-12">
 				<div id="refresh_db"></div>
 				<div id="trade-history">
-					<img id="loading-wait" src="img/loading.svg" height="50" width="50"> 
+					<img id="loading-wait" src="http://coinman.000webhostapp.com/app/img/loading.svg" height="50" width="50"> 
 					<span id="loading-text">Loading.............</span>
 				</div>
 			</div>
@@ -84,7 +65,6 @@
 				$("#loading-wait").hide();
 				$("#loading-text").hide();
 			});
-			$("#buy-sell").load("test.php");
 		};
 		window.setInterval(refreshMarket, 60000);
 		
