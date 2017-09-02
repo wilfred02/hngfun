54335ddd7254b48b4f502022f5157c08343d2929
codaphrenic <31177774+codaphrenic@users.noreply.github.com>
2017-09-02 02:29:30 +0100

    Update mail.php

--- profile/codaphrenic/mail.php
+++ profile/codaphrenic/mail.php
@@ -1,39 +1,24 @@
 <?php
-   	$admin_email = "xyluz@gmail.com";
-	if($_SERVER['REQUEST_METHOD'] == 'GET') {
-		$error          = ""; 
-		$successMessage = "";
-		$name           = $_GET['name'];
-		$to  			= 'fegoedafe@gmail.com';
-		$subject 		= $_GET['subject'];
-		$message 		= $_GET['message'];
-		$email 			= $_GET["email"];
-		
-		if (!$email) {
-			$error .= "Please enter your email address.<br>";
-		}
-		if (!$message) {
-			$error .= "The message field cannot be empty.<br>";
-		}
-		if (!$subject) {
-			$error .= "Please enter a subject.<br>";
-		}
-		if ($email && filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-			$error .= "The email address is invalid.<br>";
-		}
-		if ($error != "") {
-			$error = '<p>There were error(s) in your form:</p>' . $error;
-		}else{
-			if(empty($error)) {
-			  $config 	= include('../../config.php');
-			  $dsn		= 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-			  $con 		= new PDO($dsn, $config['username'], $config['pass']);
-			  $exe 		= $con->query('SELECT * FROM password LIMIT 1');
-			  $data		= $exe->fetch();
-			  $password = $data['password'];
-			  $message	= urlencode($message);
-			  header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$message&to=$to");
-			}
-		}
-	}
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $error 	= [];
+    $subject 	= $_POST['subject'];
+    $to  	= 'fegoedafe@gmail.com';
+    $body 	= $_POST['message'];
+  if($body == '' || $body == ' ') {
+      $error[] = 'Message cannot be empty.';
+    }
+  if($subject == '' || $subject == ' ') {
+      $error[] = 'Subject cannot be empty.';
+    }
+  if(empty($error)) {
+      $config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
+      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+      $con = new PDO($dsn, $config['username'], $config['pass']);
+      $exe = $con->query('SELECT * FROM password LIMIT 1');
+      $data = $exe->fetch();
+      $password = $data['password'];
+      $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
+      header("location: $uri");
+    }
+  }
  ?>
