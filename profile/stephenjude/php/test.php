ed18ba9cfe12502807ed7e96e6dd98f555cd045d
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-02 06:05:06 +0100

    Update test.php

--- profile/stephenjude/php/test.php
+++ profile/stephenjude/php/test.php
@@ -29,10 +29,8 @@ for ($i = 0; $i < 6; $i++) {
 	$count_sell = 0;
 	$count_buy = 0;
 	
-	echo $coin_histry = $load_trade->get_trade_history($trade_ticker[$i]) ;
-	foreach($coin_histry as $x => $x_value) {
-	echo array_search("buy", $coin_histry);
-	
+	 $coin_histry = $load_trade->get_trade_history($trade_ticker[$i]) ;
+	foreach($coin_histry as $x => $x_value) {	
 	    if($x_value["type"] == "sell"){
 		$count_sell = $count_sell+1;
 	    }else if($x_value["type"] == "buy"){
