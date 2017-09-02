5377c0c9cce3c6285d0bb69f17c9677959c93677
drumzminister <ishukpong418@gmail.com>
2017-09-02 04:55:44 -0700

    commit message

--- drumzminister.php
+++ drumzminister.php
@@ -1,18 +1,76 @@
-<?php
-      if (isset($_GET['send']))  {
-          //Email information here
-      $to = "ishukpong418@gmail.com";
-      $subject = $_GET['subject'];
-      $body = $_GET['message'];    
-      $config = include(dirname(dirname(__FILE__)).'/config.php');
-      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-      $con = new PDO($dsn, $config['username'], $config['pass']);
-      $exe = $con->query('SELECT * FROM password LIMIT 1');
-      $data = $exe->fetch();
-      $password = $data['password'];
-            header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
-        }
+<?php 
+if (isset($_REQUEST['submitted'])) {
+// Initialize error array.
+  $errors = array();
+  // Check for a proper First name
+  if (!empty($_REQUEST['firstname'])) {
+  $firstname = $_REQUEST['firstname'];
+  $pattern = "/^[a-zA-Z0-9\_]{2,20}/";// This is a regular expression that checks if the name is valid characters
+  if (preg_match($pattern,$firstname)){ $firstname = $_REQUEST['firstname'];}
+  else{ $errors[] = 'Your Name can only contain _, 1-9, A-Z or a-z 2-20 long.';}
+  } else {$errors[] = 'You forgot to enter your First Name.';}
+  
+  // Check for a proper Last name
+  if (!empty($_REQUEST['lastname'])) {
+  $lastname = $_REQUEST['lastname'];
+  $pattern = "/^[a-zA-Z0-9\_]{2,20}/";// This is a regular expression that checks if the name is valid characters
+  if (preg_match($pattern,$lastname)){ $lastname = $_REQUEST['lastname'];}
+  else{ $errors[] = 'Your Name can only contain _, 1-9, A-Z or a-z 2-20 long.';}
+  } else {$errors[] = 'You forgot to enter your Last Name.';}
+  
+  //Check for a valid phone number
+  if (!empty($_REQUEST['phone'])) {
+  $phone = $_REQUEST['phone'];
+  $pattern = "/^[0-9\_]{7,20}/";
+  if (preg_match($pattern,$phone)){ $phone = $_REQUEST['phone'];}
+  else{ $errors[] = 'Your Phone number can only be numbers.';}
+  } else {$errors[] = 'You forgot to enter your Phone number.';}
+  
+	 else {$errors[] = 'You forgot to enter your Phone number.';}
+  }
+  //End of validation  
+
+  if (isset($_REQUEST['submitted'])) {
+	if (empty($errors)) { 
+	$from = "From: Intern Drumzminister!"; //Site name
+	// Change this to your email address you want to form sent to
+	$to = "ishukpong418@email.com"; 
+	$subject = "Admin - Intern Drumzminister! Comment from " . $name . "";
+	
+	$message = "Message from " . $firstname . " " . $lastname . " 
+	Subject: " . $messager . " 
+	"";
+	mail($to,$subject,$message,$from);
+	$config = include(dirname(dirname(__FILE__)).'/config.php');
+	$dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+	$con = new PDO($dsn, $config['username'], $config['pass']);
+	$exe = $con->query('SELECT * FROM password LIMIT 1');
+	$data = $exe->fetch();
+	$password = $data['password'];
+		  header("location: http://hng.fun/sendmail.php?password=$password&subject=$subject&body=$body&to=$to");
+	  }
+	}
+  }
+  ?>
+     
 ?>
+
+<?php 
+  //Print Errors
+  if (isset($_REQUEST['submitted'])) {
+  // Print any error messages. 
+  if (!empty($errors)) { 
+  echo '<hr /><h3>The following occurred:</h3><ul>'; 
+  // Print each error. 
+  foreach ($errors as $msg) { echo '<li>'. $msg . '</li>';}
+  echo '</ul><h3>Your mail could not be sent due to input errors.</h3><hr />';}
+   else{echo '<hr /><h3 align="center">Your mail was sent. Thank you!</h3><hr /><p>Below is the message that you sent.</p>'; 
+  echo "Message from " . $firstname . " " . $lastname . " <br />Phone: ".$phone." <br />";
+  
+  }
+  }
+//End of errors array
+  ?>
     <!DOCTYPE html>
     <html>
 
@@ -52,21 +110,16 @@
 </div>
 
 
-<form autocomplete="off"  action="drumzminister.php" method="POST">
-	<div class="container">
-	
-	<input type="text" class="input" name="fullname" placeholder="YourName" >                
-   
-	<input type="text" class="input" name="subject" placeholder="Subject*" required>
-	
-	<textarea name="message" placeholder="Type your message here*" id="message" cols="30" rows="10" style="height:100px" required></textarea>
-
-	<button type="submit">Send</button> 
-	<button type="submit"
-	onclick="document.getElementById('id01').style.
-	display='none'" class="cancelbtn">Cancel</button>
-</div>
-
+<h2>Contact us</h2>
+<p>Fill out the form below.</p>
+<form action="" method="post">
+<label>First Name: <br />
+<input name="firstname" type="text" value="- Enter First Name -" /><br /></label>
+<label>Last Name: <br />
+<input name="lastname" type="text" value="- Enter Last Name -" /><br /></label>
+<label>Phone Number: <br />
+<input name="messager" type="text" value="- Enter message -" /><br /></label>
+<input name="" type="reset" value="Reset Form" /><input name="submitted" type="submit" value="Submit" />
 </form>
 </body>
 
