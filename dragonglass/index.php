68ae2cf31fddf5a430695e696391e90f81992900
Egberinde Olatunbosun <bosunski@gmail.com>
2017-09-02 20:55:59 +0100

    Update index.php

--- dragonglass/index.php
+++ dragonglass/index.php
@@ -1,30 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Load Coinman</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-</head>
-<body>
+<?php
+require('class/Poloniex.php');
+require('class/Coinman.php');
+$config = include('config.php');
 
-<div id="load-data"><!-- all data contents here--></div>
+// Crates an instance of CoinMan
+$cm = new Coinman($config['api_key'], $config['api_secret']);
 
-<script type="text/javascript">
-	$("document").ready(function (){
-		var data = 'all';
-		$.ajax({
-			type: "GET",
-			url: "class/load-data.php",
-			dataType: "json",
-			data: {
-				data: data
-			},
-			success: function(data){
-				// here data is alread in Json from the dataType set
-				// $("#load-data").html(converted json here);
-			}
-		});
-	});
-
-</script>
-</body>
-</html>
+// We Run CoinMan
+$cm->runCoinMan();
