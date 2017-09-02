c971db2fc3a5c78acce24f74ba1fc8f9915edf6a
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-02 07:50:26 +0100

    Update core.php

--- dragon/core.php
+++ dragon/core.php
@@ -65,7 +65,7 @@ for ($i = 0; $i < 6; $i++) {
 	$biggestPercent = $biggest['total_buy'] / 200 * 100;
 	echo '<div class="container">
 		  <h2>'.$biggest['coin'].'<br>
-		  <small> '.$biggest['total_buy'].' Trades</small></h2>
+		  <small> '.$biggest['total_buy'].'Bids; '.$biggest['total_buy'].' Bids</small></h2>
 		  <div class="progress">
 		    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:'.$biggestPercent.'%">
 		      <span class="sr-only">'.$biggestPercent.'% Complete</span>
