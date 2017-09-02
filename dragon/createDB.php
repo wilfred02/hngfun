e1f9c245728aef91757ff3562ba5280a3aa63c4f
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-02 12:08:13 +0100

    Update createDB.php

--- dragon/createDB.php
+++ dragon/createDB.php
@@ -5,20 +5,25 @@
   $servername = "localhost";
 $username = "root";
 $password = "@hng.fun";
+$dbname = "hng";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Create database
-$sql = "CREATE DATABASE crypto";
+// sql to create table
+$sql = "CREATE TABLE pair (
+id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+pair_name VARCHAR(30) NOT NULL
+)";
+
 if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
+    echo "Table MyGuests created successfully";
 } else {
-    echo "Error creating database: " . $conn->error;
+    echo "Error creating table: " . $conn->error;
 }
 
 $conn->close();
