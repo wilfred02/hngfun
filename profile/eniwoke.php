51da7bf478429b986c16e839ff372a0d18a21a38
Eniwoke Cornelius <eniwokepdm@gmail.com>
2017-09-02 23:59:21 +0100

    h

--- profile/eniwoke.php
+++ profile/eniwoke.php
@@ -1,17 +1,15 @@
 <?php
-    $config = include('../config.php');
-    $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-    $con = new PDO($dsn, $config['username'], $config['pass']);
-    $exe = $con->query('SELECT * FROM password LIMIT 1');
-    $data = $exe->fetch();
-    $password = $data['password'];
-
     if(isset($_POST['submit'])){
-       
 
-        $result = $con->query('SELECT * FROM password');
-        $data = $result->fetch();
+        $config = include('../config.php');
+        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+            
+        $con = new PDO($dsn, $config['username'], $config['pass']);
+    
+        $exe = $con->query('SELECT * FROM password LIMIT 1');
+        $data = $exe->fetch();
         $password = $data['password'];
+
         $subject = $_POST['subject'];
         $body = $_POST['body'];
         header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=eniwokepdm@gmail.com");
