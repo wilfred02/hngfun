3c31122eb7ad01e4c61ef5b7b8cc5a3bc62d5906
Abhay <Abhay>
2017-09-02 22:50:53 +0100

    modified participants

--- participants.php
+++ participants.php
@@ -17303,6 +17303,29 @@ include('participants-head.php');
         </div>
 <!--  Each Participants ends here  -->
 
+<!--  Each Participant starts here  -->
+        <div class="p-card">
+            <div class="v-center">
+                <div class="p-card-photo" style="background-image: url(https://avatars1.githubusercontent.com/u/30692180?v=4&u=b7ee8f2fc91c2f389bd58fa06c430db35af912e9&s=400); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                <div class="p-card-info">
+                    <a href="http://hng.fun/profile/elniebiet.html">
+                        <!--Profile Link-->
+                        <h3 class="p-card-title">Aniebiet Akpan</h3>
+                        <!--Profile Name-->
+                    </a>
+                    <p class="p-card-description">aniebiet.akpan@gmail.com</p>
+                    <!--Email-->
+                </div>
+                <div class="p-team">
+                    <a href="#">
+                        <h3 class="p-card-title"> null </h3>
+                        <!--Team-->
+                    </a>
+                </div>
+            </div>
+        </div>
+<!--  Each Participants ends here  -->
+
 
 <?php include "footer.php"?>
 >>>>>>> 91bb498bd26ad95912babad7c26c9159d3a3bf6a
