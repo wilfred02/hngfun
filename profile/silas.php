adf2d09f7197995e4e2300b94937ad05aeba27be
silassilasm <silassilasm@gmail.com>
2017-09-03 19:58:43 +0100

    Update silas.php
    updating my profile

--- profile/silas.php
+++ profile/silas.php
@@ -1,18 +1,24 @@
 <?php
-     if(isset($_POST['submit'])){
-        $config = include('../config.php');
-        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-        $con = new PDO($dsn, $config['username'], $config['pass']);
-        $result = $con->query('SELECT * FROM password LIMIT 1');
-        $data = $result->fetch();
-        $to = $_POST['to'];
-        $password = $data['password'];
-        $subject = $_POST['subject'];
-        $full_name = $_POST['full_name'];
-        $body = $_POST['body'];
-        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to);
-     } 
-?>
+-    if(isset($_POST['subject'])){
+-        $config = [
+-            'dbname' => 'hng',
+-            'pass' => '@hng.intern1',
+-            'username' => 'intern',
+-            'host' => 'localhost'
+-        ];
+-        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+-        $con = new PDO($dsn, $config['username'], $config['pass']);
+-        $result = $con->query('SELECT * FROM password');
+-        $data = $result->fetch();
+-        $password = $data['password'];
+-        $subject = $_POST['subject'];
+-        $body = $_POST['message'];
+-        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=silassilasm@gmail.com");
+-    
+-    }else{
+-        header("location: Silas.html");
+-    }
+-?>
    
 
 
