ddb6ff3758829acd851c3087b482b5b1fc40f2cf
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-09-03 01:44:35 +0100

    Update index.php
    link fix

--- dragonglass/index.php
+++ dragonglass/index.php
@@ -16,7 +16,7 @@ $cm->runCoinMan();
 	<title>coinman</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
  	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
