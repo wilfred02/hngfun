8ff9938a6b71a43ef7c73ea05bffd050b425ade6
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-03 16:09:29 +0100

    Update index2.php

--- dragonglass/index2.php
+++ dragonglass/index2.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>coinman</title>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<script src="js/jquery.js"></script>
  	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
