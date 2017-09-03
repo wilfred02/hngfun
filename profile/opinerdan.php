00a71cf6ac1b7ed02e9c8a2c2f44cd93a4eebbca
Iniobong Daniel <opinerdan@gmail.com>
2017-09-03 20:03:36 +0100

    Update opinerdan.php

--- profile/opinerdan.php
+++ profile/opinerdan.php
@@ -103,7 +103,8 @@ a {
 	  <h1>Iniobong Daniel Isang </h1>
     <p class="about"><blockquote>I am a Web Developer ,An indigine of Akwa Abasi Ibom State, Currently residing in Calabar,Cross River State </blockquote></p>
         <p><i class="fa fa-slack"></i>@opinerdan </p>
-	 <p><link class="fa-android"><link>@opinerdan</p>
+	 <p><i onClick="fa-android"></i>@opinerdan</p>
+	
         
  
     <p class="stage"><a  target="_blank" href="https://github.com/opiner/test">Stage-3</a></p>
