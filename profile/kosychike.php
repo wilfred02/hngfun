86dfe4f5471ea8fc621d13f15cd5220b39f358a3
Chike <kosyononye@gmail.com>
2017-09-03 23:14:47 +0100

    Update kosychike.php

--- profile/kosychike.php
+++ profile/kosychike.php
@@ -97,12 +97,9 @@
             <a id="twitter" href="https://twitter.com/chykeofficial">
                 <i class="fa fa-twitter"></i> twitter
 
-            </a>
-<<<<<<< HEAD
+           </a>
             
             <a href="https://drive.google.com/file/d/0BzOtc_sZyUCVamVXSHFtQVQ4VjA/view">DOWNLOAD APP</a>
-=======
->>>>>>> 4a1978c1ee18eed2b40e11dc2dd7e7b5ebbfc449
 
 
 
