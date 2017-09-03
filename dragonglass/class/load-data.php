e4e88a6c7422f96589f649979ff12c53501e7b39
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-09-03 04:26:08 +0100

    Update load-data.php
    testing Json

--- dragonglass/class/load-data.php
+++ dragonglass/class/load-data.php
@@ -11,7 +11,24 @@ $cm = new Coinman($config['api_key'], $config['api_secret']);
 // $json = $cm->runCoinMan();
 
 // decode json 
-// $json = json_decode($json, TRUE);
+$json = '{
+		"buys":[
+			{"id":"280","0":"280","pair":"BTC_NOTE","1":"BTC_NOTE","buys":"178","2":"178","sales":"22","3":"22"},
+			{"id":"259","0":"259","pair":"BTC_BTCD","1":"BTC_BTCD","buys":"164","2":"164","sales":"36","3":"36"},
+			{"id":"258","0":"258","pair":"BTC_BLK","1":"BTC_BLK","buys":"157","2":"157","sales":"43","3":"43"},
+			{"id":"302","0":"302","pair":"BTC_SC","1":"BTC_SC","buys":"156","2":"156","sales":"44","3":"44"},
+			{"id":"282","0":"282","pair":"BTC_PINK","1":"BTC_PINK","buys":"150","2":"150","sales":"50","3":"50"}],
+		"sales":[
+			{"id":"289","0":"289","pair":"BTC_VIA","1":"BTC_VIA","buys":"26","2":"26","sales":"174","3":"174"},
+			{"id":"297","0":"297","pair":"BTC_XPM","1":"BTC_XPM","buys":"32","2":"32","sales":"168","3":"168"},
+			{"id":"270","0":"270","pair":"BTC_GAME","1":"BTC_GAME","buys":"40","2":"40","sales":"160","3":"160"},
+			{"id":"318","0":"318","pair":"BTC_NXC","1":"BTC_NXC","buys":"44","2":"44","sales":"156","3":"156"},
+			{"id":"266","0":"266","pair":"BTC_DOGE","1":"BTC_DOGE","buys":"45","2":"45","sales":"155","3":"155"}]
+	}
+';
+$json = json_decode($json, TRUE);
+
+
 ?>
 
 <section>
