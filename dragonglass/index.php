7cfc710181d955134ae091eff698c950bb85f93e
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-09-03 01:45:35 +0100

    Update index.php
    changes remove

--- dragonglass/index.php
+++ dragonglass/index.php
@@ -9,17 +9,3 @@ $cm = new Coinman($config['api_key'], $config['api_secret']);
 // We Run CoinMan
 $cm->runCoinMan();
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>coinman</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-
-</body>
-</html>
