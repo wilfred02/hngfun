e1bc6bccfdfca07245569428f89750f1eeaaa5f0
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-03 16:19:23 +0100

    Update index.php

--- dragonglass/index.php
+++ dragonglass/index.php
@@ -15,11 +15,11 @@ if(isset($_GET['type'])){
 	<title>coinman</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+  	<script src="js/scripts.js"></script>
  	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  	<link rel="stylesheet" type="text/css" href="css/table-style.css">
- 	<link rel="stylesheet" type="text/css" href="js/scripts.js">
 </head>
 <body>
 	<br />
