ca81ed6f92c0b25b9f476f691e1f033fadb6b87d
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2017-09-03 11:51:20 +0100

    Update index2.php

--- dragonglass/index2.php
+++ dragonglass/index2.php
@@ -8,7 +8,6 @@
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
  	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
- 	<link rel="stylesheet" type="text/css" href="css/table-style.css">
  	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
