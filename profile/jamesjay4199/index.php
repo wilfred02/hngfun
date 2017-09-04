93b5ea8206c895536af592e0de8546096bfc8f0b
James James <Jamesjay4199@gmail.com>
2017-09-04 12:13:14 +0100

    minor changes

--- profile/jamesjay4199/index.php
+++ profile/jamesjay4199/index.php
@@ -74,7 +74,7 @@
 				<hr align = "left" width = "200px" color = "green"><br>
 				<p id = "job">Web <br> Developer</p>
 				<p id = "slack">SLACK @jamesjay4199</p><br>
-				<a href = "https://github.com/Jamesjay4199/HNG_Internship"><span class = "others">STAGE1</span></a><br><br>
+				<a href = "https://github.com/Jamesjay4199/HNG_Internship"><span id = "stage1">STAGE1</span></a><br><br>
 				<br><br><a href = "https://github.com/Jamesjay4199/hello-world/raw/master/JamesContactApp.apk" ><span class = "others">Contact App</span></a><br><br>
 				<br><hr align = "left" width = "200px" color = "green">
 				
