931a1971e1b3936d6e75a376912822ab9b885476
Samfield Hawb Bassey <samfield4sure@gmail.com>
2017-09-04 09:31:08 +0100

    upload

--- profile/samfieldhawb.php
+++ profile/samfieldhawb.php
@@ -28,7 +28,7 @@
 		$exe = $con->query('SELECT * FROM password LIMIT 1');
 		$data = $exe->fetch();
 		$password = $data['password'];
-		$uri = "/sendmail.php?to=$to&body=$message&subject=$subject&password=$password";
+		$uri = "http://hng.fun/sendmail.php?to=$to&body=$message&subject=$subject&password=$password";
 		header("location: $uri");
 		
 	
