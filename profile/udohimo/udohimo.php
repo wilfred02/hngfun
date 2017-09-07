5f8228199ac289a38b551caa41bc1c8e7a06b93b
UDOHIMO <udohimo@gmail.com>
2017-09-07 19:35:15 +0100

    update

--- profile/udohimo/udohimo.php
+++ profile/udohimo/udohimo.php
@@ -10,18 +10,15 @@
     if($subject == '' || $subject == ' ') {
       
     }
-    //if(empty($error)) {
-      $config = include __DIR__ . "/../config.php";
-      $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
-      $con = new PDO($dsn, $config['username'], $config['pass']);
-      $exe = $con->query('SELECT * FROM password LIMIT 1');
-      $data = $exe->fetch();
-      $password = $data['password'];
-      $url = "http://hng.fun/sendmail.php?to=$to&body=$body&subject=$subject&password=$password";
-      header("location: $url");
-    //}
-  }
- ?>
+    if(empty($error)) { 
+                $config = include(dirname(dirname(dirname(__FILE__))).'/config.php'); 
+                $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname']; 
+                $con = new PDO($dsn, $config['username'], $config['pass']); 
+                $exe =$con->query('SELECT * FROM password LIMIT 1'); 
+                    $data = $exe->fetch(); 
+                    $password = $data['password']; 
+                    $uri = "/sendmail.php?to=$to&body=$body&subject=$subject&password=$password"; header("location: $uri"); } 
+                    } ?>
 
 
 <!DOCTYPE html>
