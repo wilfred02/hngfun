3200dc32827f5d307ab04ab82b0c2afca17da681
Starlyvil <31422222+Starlyvil@users.noreply.github.com>
2017-09-08 09:38:56 +0100

    Fixing corrupt all corrupt files made by some one

--- profile/starlyvil/ini/conn.php
+++ profile/starlyvil/ini/conn.php
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 <?php
 	define("HOST", "localhost");
 	define("DB", "hng");
 	define("USER", "intern");
 	define("PASSWORD", "@hng.intern1");
-=======
-<?php
-	define("HOST", "localhost");
-	define("DB", "hng");
-	define("USER", "intern");
-	define("PASSWORD", "@hng.intern1");
->>>>>>> 5815338a02e29a6390f818715b29681d77ca886a
 ?>
